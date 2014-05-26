d33e77405f3db861d70cad8109550239c32df4ec
jimmydorry <jimmydorry+github@gmail.com>
2014-05-26 16:21:28 +1000

    Formatting in game_servers page

--- game_servers.php
+++ game_servers.php
@@ -2,8 +2,10 @@
 
 <h3>Current Servers</h3>
 <h4><a href="http://d2modd.in">D2Moddin</a></h4>
-<p>We currently host servers in North American and Australia for D2Moddin. Support is provided via the Official <a target="_blank" href="http://steamcommunity.com/groups/D2Moddin/discussions">Steam Community group</a>.</p>
+<p>We currently host servers in North American and Australia for D2Moddin.</p>
+<p>Support is provided via the Official <a target="_blank" href="http://steamcommunity.com/groups/D2Moddin/discussions">Steam Community group</a>.</p>
 
 <h3>Previous Servers</h3>
 <h4><a href="http://d2ware.net">D2Ware</a></h4>
-<p>We hosted servers in Australia for D2Ware. Support for them is provided via the Official <a target="_blank" href="http://steamcommunity.com/groups/D2Ware/discussions">Steam Community group</a>.</p>
+<p>We hosted servers in Australia for D2Ware.</p>
+<p>Support for them is provided via the Official <a target="_blank" href="http://steamcommunity.com/groups/D2Ware/discussions">Steam Community group</a>.</p>
