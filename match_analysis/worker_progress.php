1324623934d206b752a5ff761bdb7488165871ce
jimmydorry <jimmydorry+github@gmail.com>
2014-05-27 13:32:48 +1000

    Un-do formatting

--- match_analysis/worker_progress.php
+++ match_analysis/worker_progress.php
@@ -33,21 +33,21 @@ try {
         $tasks_total_format = number_format($d2_worker_task_count[0]['total_tasks']);
         $tasks_completed_format = number_format($d2_worker_task_count_completed[0]['total_tasks']);
         $tasks_completed_per_format = number_format($d2_worker_task_count_completed[0]['total_tasks'] / $d2_worker_task_count[0]['total_tasks'] * 100, 2) . '%';
-        echo '<span class="h5">Tasks:</span> ' . $tasks_completed_format . ' / ' . $tasks_total_format . ' (' . $tasks_completed_per_format . ')<br />';
+        echo '<span class="h4">Tasks:</span> ' . $tasks_completed_format . ' / ' . $tasks_total_format . ' (' . $tasks_completed_per_format . ')<br />';
 
         $time_taken = (strtotime($d2_worker_task_count[0]['latest_date']) - strtotime($d2_worker_task_count[0]['earliest_date'])) / 60 / 60 / 24;
-        echo '<span class="h5">Total Uptime:</span> ' . number_format($time_taken, 2) . 'days<br />';
+        echo '<span class="h4">Total Uptime:</span> ' . number_format($time_taken, 2) . 'days<br />';
 
         if ($d2_worker_task_count_completed[0]['total_tasks'] <= 0) $d2_worker_task_count_completed[0]['total_tasks'] = 1;
         $estimated_time = ($d2_worker_task_count[0]['total_tasks'] / $d2_worker_task_count_completed[0]['total_tasks']) * $time_taken;
-        echo '<span class="h5">Estimated Time Left:</span> ' . number_format($estimated_time - $time_taken, 2) . 'days (Total of ' . number_format($estimated_time, 2) . 'days.)<br />';
+        echo '<span class="h4">Estimated Time Left:</span> ' . number_format($estimated_time - $time_taken, 2) . 'days (Total of ' . number_format($estimated_time, 2) . 'days.)<br />';
 
         echo '<br />';
 
         $time_taken_l10 = (strtotime($d2_worker_task_completed_last10[0]['max_date']) - strtotime($d2_worker_task_completed_last10[0]['min_date'])) / 60 / 60 / 24;
         $estimated_time_l10 = $time_taken_l10 / 20 * ($d2_worker_task_count[0]['total_tasks'] - $d2_worker_task_count_completed[0]['total_tasks']);
-        echo '<span class="h5">Time Required for last 20tasks:</span> ' . number_format($time_taken_l10, 2) . 'days<br />';
-        echo '<span class="h5">Estimated Time Left (last 20 tasks):</span> ' . number_format($estimated_time_l10 - $time_taken_l10, 2) . 'days<br />';
+        echo '<span class="h4">Time Required for last 20tasks:</span> ' . number_format($time_taken_l10, 2) . 'days<br />';
+        echo '<span class="h4">Estimated Time Left (last 20 tasks):</span> ' . number_format($estimated_time_l10 - $time_taken_l10, 2) . 'days<br />';
 
         //////////////////////////////////////////////
 
