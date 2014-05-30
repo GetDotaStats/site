82aa7679d62d9a92937dfd83c2d314bb089c5e74
jimmydorry <jimmydorry+github@gmail.com>
2014-05-30 14:17:13 +1000

    Updated DBE description

--- dbe/index.php
+++ dbe/index.php
@@ -1,13 +1,17 @@
-<p>The DotaBuff thread can be found here: <a
+<p>The Dotabuff thread can be found here: <a
         href="http://dotabuff.com/topics/2013-03-05-dotabuff-extended-mozilla-addon">http://dotabuff.com/topics/2013-03-05-dotabuff-extended-mozilla-addon</a>
 </p>
-<p>This plugin adds the following to DotaBuff pages:</p>
+<p>This plugin adds the following to Dotabuff pages:</p>
 <ul>
     <li>Signature generator tab on user profiles</li>
     <li>Tower & Barracks Status tab on match detail pages</a></li>
+    <li>A signature under each post on Dotabuff forums</a></li>
 </ul>
-<p><a href="./dbe/images/1.png" target="__new"><img src="./dbe/images/1.png" width="100px"/></a> <a
-        href="./dbe/images/2.png" target="__new"><img src="./dbe/images/2.png" width="100px"/></a></p>
+<p>
+    <a href="./dbe/images/1.png" target="__new"><img src="./dbe/images/thumbnails/1.png"/></a>
+    <a href="./dbe/images/2.png" target="__new"><img src="./dbe/images/thumbnails/2.png"/></a>
+    <a href="./dbe/images/3.png" target="__new"><img src="./dbe/images/thumbnails/3.png"/></a>
+</p>
 <h2>Download Links</h2>
 <p><a href="https://chrome.google.com/webstore/detail/dotabuff-extended/oggpckdeaofelnblpijaiijmgfckncdf?hl=en&gl=AU">Chrome
         Extension</a></p>
@@ -23,11 +27,11 @@
     <li>v 0.4.3 - Removed hero_dmg, hero_heal, tower_dmg</li>
     <li>v 0.4.2 - Some links changed to point new domain</li>
     <li>v 0.4 - Tower and Barracks Status is back :)</li>
-    <li>v 0.3.2 - Fixed bug that causes addon didn't work for some time. Tower and Barracks Status still not working.
+    <li>v 0.3.2 - Fixed bug that causes extension didn't work for some time. Tower and Barracks Status still not working
     </li>
     <li>v 0.2 - Added Tower and Barracks Status</li>
     <li>v 0.1 - First release</li>
 </ul>
 <h2>Credits</h2>
 <p>Originally developed by gmilanche (gmilanche.gm AT gmail DOT com). Proudly hosted by GetDotaStats. Chrome plugin
-    maintained by GetDotaStats</p>
+    maintained and improved by GetDotaStats</p>
