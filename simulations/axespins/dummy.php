82cc16765288f75a746b467636be770f2e3796f7
jimmydorry <jimmydorry+github@gmail.com>
2014-06-11 14:34:40 +1000

    Roll out dummy

--- simulations/axespins/dummy.php
+++ simulations/axespins/dummy.php
@@ -1,5 +1,16 @@
 <?php
 
-//header("Location: ../../#simulations__axespins/?");
+$query = '';
+$i = 0;
+foreach($_POST as $key => $value){
+    if($i == 0){
+        $query .= $key . '=' . $value;
+    }
+    else{
+        $query .= '&'.$key . '=' . $value;
+    }
+    $i++;
+}
 
-echo json_encode($_POST);
+//echo json_encode($_POST);
+header("Location: ../../#simulations__axespins/?".$query);
