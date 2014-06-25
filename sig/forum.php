bec3ad33df12ad6c8b0b28d786302a5bca58fa13
jimmydorry <jimmydorry+github@gmail.com>
2014-06-25 11:31:01 +1000

    Another fix for signatures

--- sig/forum.php
+++ sig/forum.php
@@ -198,8 +198,8 @@ if(!file_exists($file_name_location) || (filemtime($file_name_location) <= strto
 			if(!empty($mmr_stats[0]['dota_wins'])){
 				$dota_wins = $mmr_stats[0]['dota_wins'];
 			}
-			else if(!empty($sig_stats_winrate['acount_win'])){
-				$dota_wins = $sig_stats_winrate['acount_win'];
+			else if(!empty($sig_stats_winrate['account_win'])){
+				$dota_wins = $sig_stats_winrate['account_win'];
 			}
 			else{
 				$dota_wins = '???';
@@ -243,7 +243,7 @@ if(!file_exists($file_name_location) || (filemtime($file_name_location) <= strto
 			}
 
 			$text_colour = imagecolorallocate( $base_img, 78, 213, 84 );
-			//$overlay_text = $sig_stats_winrate['acount_win'] . ' wins';
+			//$overlay_text = $sig_stats_winrate['account_win'] . ' wins';
 			$tb = imagettfbbox($font_size, 0, $font_norm, $overlay_text);
 
 			$overlay_text_offset_x = 10;
