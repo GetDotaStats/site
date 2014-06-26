625b0e207a30d225e1319086e48f1e1ddba82500
jimmydorry <jimmydorry+github@gmail.com>
2014-06-26 10:53:10 +1000

    Update dir location of routine

--- d2moddin/routine/scripts/15mins.sh
+++ d2moddin/routine/scripts/15mins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ##every 15mins
-cd /home/www-getdotastats-staging/d2moddin/routine/php/
+cd /home/www-getdotastats/d2moddin/routine/php/
 echo "Run at: $(date -u)<br />"
 echo "-------------------<br />"
 ./15mins.php
