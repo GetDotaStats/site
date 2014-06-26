ddb7082abd57c73e48c3b67bec8c3bf1899fdb62
jimmydorry <jimmydorry+github@gmail.com>
2014-06-26 10:40:08 +1000

    Update line endings, remove $_GET requirement for console version

--- d2moddin/routine/scripts/15mins.sh
+++ d2moddin/routine/scripts/15mins.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ##every 15mins
 cd /home/www-getdotastats-staging/d2moddin/routine/php/
-echo "Run at: $(date -u)"
-echo "-------------------"
-./daily_queries.php
-echo "-------------------"
+echo "Run at: $(date -u)<br />"
+echo "-------------------<br />"
+./15mins.php
+echo "-------------------<br />"
 echo "Run at: $(date -u)"
