f48f0f9743e63d76a970f352566bef9c0f837f1e
jimmydorry <jimmydorry@users.noreply.github.com>
2014-07-01 07:51:43 +1000

    Update URL

--- d2moddin/routine/php/15mins.php
+++ d2moddin/routine/php/15mins.php
@@ -6,7 +6,7 @@ require_once('../../functions.php');
 try {
     $db = new dbWrapper($hostname_d2moddin, $username_d2moddin, $password_d2moddin, $database_d2moddin, true);
 
-    $stats = json_decode(curl('http://ddp2.d2modd.in/stats/general', NULL, NULL, NULL, NULL, 20), 1);
+    $stats = json_decode(curl('http://net1.d2modd.in/stats/general', NULL, NULL, NULL, NULL, 20), 1);
 
     ////////////////////////
     //GENERAL STATS
