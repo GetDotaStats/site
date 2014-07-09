ee43d3b0c87daa72a156fdf985a77523594d0ccd
jimmydorry <jimmydorry+github@gmail.com>
2014-07-10 09:27:53 +1000

    Add mod stats and fixed server stats (schema changed)

--- d2moddin/routine/php/15mins.php
+++ d2moddin/routine/php/15mins.php
@@ -1,59 +1,93 @@
 #!/usr/bin/php -q
 <?php
 require_once('../../../connections/parameters.php');
-require_once('../../functions.php');
+require_once('../../../global_functions.php');
 
 try {
     $db = new dbWrapper($hostname_d2moddin, $username_d2moddin, $password_d2moddin, $database_d2moddin, true);
 
-    $stats = json_decode(curl('http://net1.d2modd.in/stats/general', NULL, NULL, NULL, NULL, 20), 1);
-
     ////////////////////////
     //GENERAL STATS
     ////////////////////////
-    $db->q("INSERT INTO `stats_production` (`lobby_total`, `lobby_wait`, `lobby_play`, `lobby_queue`) VALUES (?, ?, ?, ?)",
-        "iiii",
-        $stats['lobby_total'], $stats['lobby_wait'], $stats['lobby_play'], $stats['lobby_queue']);
+    {
+        $stats = json_decode(curl('http://net1.d2modd.in/stats/general', NULL, NULL, NULL, NULL, 20), 1);
 
-    ////////////////////////
-    //REGIONS
-    ////////////////////////
-    $sql = array();
-    foreach ($stats['regions'] as $key => $value) {
-        if (isset($value['name']) && $value['id'] && $value['servercount'] && $value['playing']) {
-            $sql[] = '(\'' . $db->escape($value['name']) . '\', ' .
-                $db->escape($value['id']) . ', ' .
-                $db->escape($value['servercount']) . ', ' .
-                $db->escape($value['playing']) . ')';
+        $db->q("INSERT INTO `stats_production` (`lobby_total`, `lobby_wait`, `lobby_play`, `lobby_queue`) VALUES (?, ?, ?, ?)",
+            "iiii",
+            $stats['lobby_total'], $stats['lobby_wait'], $stats['lobby_play'], $stats['lobby_queue']);
+
+        ////////////////////////
+        //REGIONS
+        ////////////////////////
+        $sql = array();
+        foreach ($stats['regions'] as $key => $value) {
+            if (isset($value['name']) && isset($value['id']) && isset($value['servercount']) && isset($value['playing'])) {
+                $sql[] = '(\'' . $db->escape($value['name']) . '\', ' .
+                    $db->escape($value['id']) . ', ' .
+                    $db->escape($value['servercount']) . ', ' .
+                    $db->escape($value['playing']) . ')';
+            } else {
+                echo 'Failed: ' . $value['name'] . '<br />';
+            }
         }
-    }
-    $sql_values = implode(', ', $sql);
-    unset($sql);
+        $sql_values = implode(', ', $sql);
+        unset($sql);
 
-    $db->q("INSERT INTO `stats_production_regions` (`region_name`, `region_id`, `region_servercount`, `region_playing`) VALUES " . $sql_values);
+        $db->q("INSERT INTO `stats_production_regions` (`region_name`, `region_id`, `region_servercount`, `region_playing`) VALUES " . $sql_values);
 
-    ////////////////////////
-    //SERVERS
-    ////////////////////////
-    $sql = array();
-    foreach ($stats['servers'] as $key => $value) {
-        if (isset($value['region']) && isset($value['name']) && isset($value['ip']) && isset($value['activeinstances']) && isset($value['maxinstances'])) {
-            $sql[] = '(' .
-                $db->escape($value['region']) . ', \'' .
-                $db->escape($value['name']) . '\', \'' .
-                $db->escape($value['ip']) . '\', ' .
-                $db->escape($value['activeinstances']) . ', ' .
-                $db->escape($value['maxinstances']) . ')';
+        ////////////////////////
+        //SERVERS
+        ////////////////////////
+        $sql = array();
+        foreach ($stats['servers'] as $key => $value) {
+            if (isset($value['region']) && isset($value['name']) && isset($value['ip']) && isset($value['activeinstances']) && isset($value['maxinstances'])) {
+                foreach ($value['region'] as $key2 => $value2) {
+                    $sql[] = '(' .
+                        $db->escape($value2) . ', \'' .
+                        $db->escape($value['name']) . '\', \'' .
+                        $db->escape($value['ip']) . '\', ' .
+                        $db->escape($value['activeinstances']) . ', ' .
+                        $db->escape($value['maxinstances']) . ')';
+                }
+            } else {
+                echo 'Failed: ' . $value['name'] . ' | ' . $value['region'] . '<br />';
+            }
         }
+        $sql_values = implode(', ', $sql);
+        unset($sql);
+
+        $db->q("INSERT INTO `stats_production_servers` (`region_id`, `server_name`, `server_ip`, `server_activeinstances`, `server_maxinstances`) VALUES " . $sql_values);
+
+        echo '<pre>';
+        print_r($stats);
+        echo '</pre>';
     }
-    $sql_values = implode(', ', $sql);
-    unset($sql);
 
-    $db->q("INSERT INTO `stats_production_servers` (`region_id`, `server_name`, `server_ip`, `server_activeinstances`, `server_maxinstances`) VALUES " . $sql_values);
+    echo '<hr />';
 
-    echo '<pre>';
-    print_r($stats);
-    echo '</pre>';
+    ////////////////////////
+    //MODS
+    ////////////////////////
+    {
+        $stats = json_decode(curl('http://net1.d2modd.in/stats/mods', NULL, NULL, NULL, NULL, 20), 1);
+
+        $sql = array();
+        foreach ($stats['mods'] as $key => $value) {
+            if (isset($value['name']) && isset($value['version']) && isset($value['lobbies'])) {
+                $sql[] = '(\'' . $db->escape($value['name']) . '\', \'' .
+                    $db->escape($value['version']) . '\', ' .
+                    $db->escape($value['lobbies']) . ')';
+            }
+        }
+        $sql_values = implode(', ', $sql);
+        unset($sql);
+
+        $db->q("INSERT INTO `stats_production_mods` (`mod_name`, `mod_version`, `mod_lobbies`) VALUES " . $sql_values);
+
+        echo '<pre>';
+        print_r($stats);
+        echo '</pre>';
+    }
 } catch (Exception $e) {
     echo $e->getMessage();
 }
