d3cc0d78916fc2d8ef4905b0b903e036ec053102
jimmydorry <jimmydorry+github@gmail.com>
2014-07-28 09:44:51 +1000

    Update game_server page

--- game_servers.php
+++ game_servers.php
@@ -2,8 +2,8 @@
 
 <h3>Current Servers</h3>
 <h4><a href="http://d2modd.in">D2Moddin</a></h4>
-<p>We currently host servers in North American and Australia for D2Moddin. We also created and host the queue for entering the closed Beta.</p>
-<p>Support is provided via the Official <a target="_blank" href="http://steamcommunity.com/groups/D2Moddin/discussions">Steam Community group</a>.</p>
+<p>We currently host servers in North America for D2Moddin. We also created and host the queue for entering the closed Beta (no longer required). At one point we also provided Australian servers, but have since had to withdraw that offering.</p>
+<p>Support is provided via the Official <a target="_blank" href="http://forum.d2modd.in/">Forums</a>.</p>
 
 <h3>Previous Servers</h3>
 <h4><a href="http://d2ware.net">D2Ware</a></h4>
