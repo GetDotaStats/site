85b91376ad0875add8aedd7d55d16b1066a0bc80
jimmydorry <jimmydorry+github@gmail.com>
2014-09-25 09:22:54 +1000

    Update list_messages adding some crude parsing

--- d2mods/list_messages.php
+++ d2mods/list_messages.php
@@ -53,24 +53,82 @@ if (isset($_COOKIE['session']) && empty($_SESSION['user_id64'])) {
                 $db = new dbWrapper_v2($hostname_gds_site, $username_gds_site, $password_gds_site, $database_gds_site);
                 if ($db) {
                     $messages = $db->q('SELECT * FROM `node_listener` ORDER BY date_recorded DESC;');
+                    $mods = $db->q('SELECT * FROM `mod_list`;');
 
-                    echo '<div class="table-responsive">
-                    <table class="table table-striped table-hover" style="word-wrap:break-word; table-layout:fixed;">';
-                    echo '<tr>
+                    $modList = array();
+                    foreach ($mods as $key => $value) {
+                        $modList[$value['mod_identifier']]['mod_name'] = $value['mod_name'];
+                        $modList[$value['mod_identifier']]['mod_workshop_link'] = $value['mod_workshop_link'];
+                    }
+
+                    $modCount = array();
+
+                    $table = '';
+                    $table .= '<div class="table-responsive">
+                        <table class="table table-striped table-hover" style="word-wrap:break-word; table-layout:fixed;">';
+                    $table .= '<tr>
                             <th width="50">&nbsp;</th>
                             <th>Message</th>
+                            <th width="150">Mod</th>
+                            <th width="100">Match ID</th>
+                            <th width="70">Players</th>
                             <th width="115">IP</th>
                             <th width="120">Recorded</th>
                         </tr>';
                     foreach ($messages as $key => $value) {
-                        echo '<tr>
+                        $json = json_decode($value['message'], 1);
+
+                        $modName = !empty($json)
+                            ? $modList[$json['modID']]['mod_name']
+                            : '??';
+
+                        $modMatchId = !empty($json)
+                            ? $json['matchID']
+                            : '??';
+
+                        $modPlayers = !empty($json)
+                            ? count($json['rounds']['players'])
+                            : '??';
+
+                        if (!empty($modCount[$modName])) {
+                            $modCount[$modName]++;
+                        } else {
+                            $modCount[$modName] = 1;
+                        }
+
+                        $table .= '<tr>
                             <td>' . $value['test_id'] . '</td>
                             <td>' . stripslashes($value['message']) . '</td>
+                            <td>' . $modName . '</td>
+                            <td>' . $modMatchId . '</td>
+                            <td class="text-center">' . $modPlayers . '</td>
                             <td>' . $value['remote_ip'] . '<br />' . $value['remote_port'] . '</td>
                             <td>' . relative_time($value['date_recorded']) . '</td>
                         </tr>';
                     }
-                    echo '</table></div>';
+                    $table .= '</table></div>';
+
+                    $table2 = '';
+                    $table2 .= '<div class="col-sm-6">
+                        <div class="table-responsive">
+                        <table class="table table-striped table-hover table-condensed" style="width: auto !important">';
+                    $table2 .= '<tr>
+                            <th>Mod</th>
+                            <th>Frequency</th>
+                        </tr>';
+                    foreach ($modCount as $key => $value) {
+                        $table2 .= '<tr>
+                            <td>' . $key . '</td>
+                            <td class="text-center">' . $value . '</td>
+                        </tr>';
+                    }
+                    $table2 .= '</table></div></div>';
+
+                    echo $table2;
+
+                    echo '<br />';
+
+                    echo $table;
 
                 } else {
                     echo '<div class="page-header"><div class="alert alert-danger" role="alert"><strong>Oh Snap:</strong> No DB!</div></div>';
