08e780c9b03aaf10414acb04e5aa3ab0973c7c73
jimmydorry <your.name@domain>
2014-10-04 16:45:21 +1000

    Improve readability and add header before game check

--- d2mods/stats.php
+++ d2mods/stats.php
@@ -26,8 +26,10 @@ try {
             );
 
             if (!empty($modDetails)) {
+                echo '<h2>' . $modDetails[0]['mod_name'] . '</h2>';
+                echo '<p><a class="nav-clickable" href="#d2mods__directory">Back to Mod Directory</a></p>';
+
                 $modRange = $db->q(
-                //'SELECT * FROM `stats_mods_duration` ORDER BY `mod`, `range_end`;',
                     'SELECT
                             MIN(mmo.`match_recorded`) as date_end,
                             MAX(mmo.`match_recorded`) as date_start
@@ -40,169 +42,160 @@ try {
                 );
 
                 if (!empty($modRange['date_end'])) {
-                    echo '<h2>' . $modDetails[0]['mod_name'] . '</h2>';
-                    echo '<p><a class="nav-clickable" href="#d2mods__directory">Back to Mod Directory</a></p>';
 
-                    ////////////////////////////////////////////////////////
-                    // LAST WEEK
-                    ////////////////////////////////////////////////////////
+                    /*$db -> q('CREATE TABLE IF NOT EXISTS `stats_mods_duration` SELECT
+                        300 * floor(`duration` / 300) as `range_start`,
+                        300 * floor(`duration` / 300) + 300 as `range_end`,
+                        COUNT(*) as `num_games`,
+                        `mod`
+                    FROM `match_stats`
+                    GROUP BY `mod`, 2
+                    ORDER BY `mod`, 2;');*/
 
-                    {
+                    $modStats = $db->q(
+                        'SELECT
+                            240 * floor(mmo.`match_duration` / 240) as `range_start`,
+                            240 * floor(mmo.`match_duration` / 240) + 240 as `range_end`,
+                            COUNT(*) as `num_games`
+                        FROM `mod_match_overview` mmo
+                        LEFT JOIN `mod_list` ml ON ml.mod_identifier = mmo.mod_id
+                        WHERE ml.`mod_id` = ?
+                        GROUP BY 2
+                        ORDER BY 2;',
+                        'i',
+                        $modID
+                    );
 
-                        /*$db -> q('CREATE TABLE IF NOT EXISTS `stats_mods_duration` SELECT
-                            300 * floor(`duration` / 300) as `range_start`,
-                            300 * floor(`duration` / 300) + 300 as `range_end`,
-                            COUNT(*) as `num_games`,
-                            `mod`
-                        FROM `match_stats`
-                        GROUP BY `mod`, 2
-                        ORDER BY `mod`, 2;');*/
+                    $testArray = array();
+                    $lastNum = 0; //NEED TO BE NEGATIVE TO GRAPH 0 TOO
 
-                        $modStats = $db->q(
-                            'SELECT
-                                240 * floor(mmo.`match_duration` / 240) as `range_start`,
-                                240 * floor(mmo.`match_duration` / 240) + 240 as `range_end`,
-                                COUNT(*) as `num_games`
-                            FROM `mod_match_overview` mmo
-                            LEFT JOIN `mod_list` ml ON ml.mod_identifier = mmo.mod_id
-                            WHERE ml.`mod_id` = ?
-                            GROUP BY 2
-                            ORDER BY 2;',
-                            'i',
-                            $modID
-                        );
+                    $durationArray = '';
 
-                        $testArray = array();
-                        $lastNum = 0; //NEED TO BE NEGATIVE TO GRAPH 0 TOO
+                    $periodGrouping = 4; //CHANGE SQL TOO IF YOU MODIFY THIS
+                    $periodCutoff = 48;
 
-                        $durationArray = '';
+                    foreach ($modStats as $key => $value) {
+                        $value['range_end'] = $value['range_end'] / 60;
 
-                        $periodGrouping = 4; //CHANGE SQL TOO IF YOU MODIFY THIS
-                        $periodCutoff = 48;
-
-                        foreach ($modStats as $key => $value) {
-                            $value['range_end'] = $value['range_end'] / 60;
-
-                            if ($value['range_end'] > $periodCutoff) {
-                                if (empty($testArray[$periodCutoff . '+'])) {
-                                    $testArray[$periodCutoff . '+'] = $value['num_games'];
-                                } else {
-                                    $testArray[$periodCutoff . '+'] += $value['num_games'];
-                                }
+                        if ($value['range_end'] > $periodCutoff) {
+                            if (empty($testArray[$periodCutoff . '+'])) {
+                                $testArray[$periodCutoff . '+'] = $value['num_games'];
                             } else {
-                                if ($value['range_end'] > ($lastNum + $periodGrouping)) {
-                                    while ($value['range_end'] > ($lastNum + $periodGrouping)) {
-                                        $testArray[$lastNum . ' - ' . ($lastNum + $periodGrouping)] = 0;
-                                        $lastNum += $periodGrouping;
-                                    }
+                                $testArray[$periodCutoff . '+'] += $value['num_games'];
+                            }
+                        } else {
+                            if ($value['range_end'] > ($lastNum + $periodGrouping)) {
+                                while ($value['range_end'] > ($lastNum + $periodGrouping)) {
+                                    $testArray[$lastNum . ' - ' . ($lastNum + $periodGrouping)] = 0;
+                                    $lastNum += $periodGrouping;
                                 }
-
-                                $testArray[$lastNum . ' - ' . $value['range_end']] = $value['num_games'];
-
-                                $lastNum = $value['range_end'];
                             }
 
-                            if (isset($durationArray)) {
-                                $durationArray += ($value['range_end'] * $value['num_games']);
-                            } else {
-                                $durationArray = ($value['range_end'] * $value['num_games']);
-                            }
+                            $testArray[$lastNum . ' - ' . $value['range_end']] = $value['num_games'];
+
+                            $lastNum = $value['range_end'];
                         }
 
-                        /*echo '<pre>';
-                        print_r($testArray);
-                        echo '</pre>';
-                        //exit();*/
-
-
-                        $options = array(
-                            //'title' => 'Average spins in ' . $hits . ' attacks',
-                            //'theme' => 'maximized',
-                            'bar' => array(
-                                'groupWidth' => 3,
-                            ),
-                            'height' => 400,
-                            'chartArea' => array(
-                                'width' => '100%',
-                                'height' => '80%',
-                                'left' => 50,
-                                'top' => 10,
-                            ),
-                            'hAxis' => array(
-                                'title' => 'Duration',
-                                //'maxAlternation' => 1,
-                                //'textPosition' => 'none',
-                                //'textPosition' => 'in',
-                                //'viewWindowMode' => 'maximized'
-                                'slantedText' => 1,
-                                'slantedTextAngle' => 60,
-                            ),
-                            'vAxis' => array(
-                                'title' => 'Games',
-                                //'textPosition' => 'in',
-                                'logScale' => 1,
-                            ),
-                            'legend' => array(
-                                'position' => 'none',
-                            ),
-                            'seriesType' => "bars",
-                            'tooltip' => array(
-                                'isHtml' => 1,
-                            ),
-                        );
-
-                        $optionsDataTable = array(
-                            'sortColumn' => 0,
-                            'sortAscending' => true,
-                            'alternatingRowStyle' => true,
-                            'page' => 'enable',
-                            'pageSize' => 5);
-
-
-                        $chart = new chart2('ComboChart');
-
-                        $super_array = array();
-                        foreach ($testArray as $key2 => $value2) {
-                            $super_array[] = array('c' => array(array('v' => $key2), array('v' => $value2), array('v' => '<strong>' . $key2 . '</strong> mins<br />Games: <strong>' . $value2 . '</strong>')));
+                        if (isset($durationArray)) {
+                            $durationArray += ($value['range_end'] * $value['num_games']);
+                        } else {
+                            $durationArray = ($value['range_end'] * $value['num_games']);
                         }
+                    }
 
-                        $data = array(
-                            'cols' => array(
-                                array('id' => '', 'label' => 'Duration', 'type' => 'string'),
-                                array('id' => '', 'label' => 'Games', 'type' => 'number'),
-                                array('id' => '', 'label' => 'Tooltip', 'type' => 'string', 'role' => 'tooltip', 'p' => array('html' => 1)),
-                            ),
-                            'rows' => $super_array
-                        );
-
-                        end($value);
-                        $maxKey = key($value);
+                    /*echo '<pre>';
+                    print_r($testArray);
+                    echo '</pre>';
+                    //exit();*/
 
 
-                        $chart_width = max(count($super_array) * 5, 400);
-                        $options['width'] = $chart_width;
-                        $options['hAxis']['maxValue'] = $maxKey + 2;
-                        $options['hAxis']['gridlines']['count'] = ($maxKey + 2) / 2;
+                    $options = array(
+                        //'title' => 'Average spins in ' . $hits . ' attacks',
+                        //'theme' => 'maximized',
+                        'bar' => array(
+                            'groupWidth' => 3,
+                        ),
+                        'height' => 400,
+                        'chartArea' => array(
+                            'width' => '100%',
+                            'height' => '80%',
+                            'left' => 50,
+                            'top' => 10,
+                        ),
+                        'hAxis' => array(
+                            'title' => 'Duration',
+                            //'maxAlternation' => 1,
+                            //'textPosition' => 'none',
+                            //'textPosition' => 'in',
+                            //'viewWindowMode' => 'maximized'
+                            'slantedText' => 1,
+                            'slantedTextAngle' => 60,
+                        ),
+                        'vAxis' => array(
+                            'title' => 'Games',
+                            //'textPosition' => 'in',
+                            'logScale' => 1,
+                        ),
+                        'legend' => array(
+                            'position' => 'none',
+                        ),
+                        'seriesType' => "bars",
+                        'tooltip' => array(
+                            'isHtml' => 1,
+                        ),
+                    );
 
-                        echo '<h3>Games Played per Duration</h3>';
-                        echo '<div id="duration_breakdown" style="width: 400px;"></div>';
+                    $optionsDataTable = array(
+                        'sortColumn' => 0,
+                        'sortAscending' => true,
+                        'alternatingRowStyle' => true,
+                        'page' => 'enable',
+                        'pageSize' => 5);
 
-                        echo '<div class="container">
+
+                    $chart = new chart2('ComboChart');
+
+                    $super_array = array();
+                    foreach ($testArray as $key2 => $value2) {
+                        $super_array[] = array('c' => array(array('v' => $key2), array('v' => $value2), array('v' => '<strong>' . $key2 . '</strong> mins<br />Games: <strong>' . $value2 . '</strong>')));
+                    }
+
+                    $data = array(
+                        'cols' => array(
+                            array('id' => '', 'label' => 'Duration', 'type' => 'string'),
+                            array('id' => '', 'label' => 'Games', 'type' => 'number'),
+                            array('id' => '', 'label' => 'Tooltip', 'type' => 'string', 'role' => 'tooltip', 'p' => array('html' => 1)),
+                        ),
+                        'rows' => $super_array
+                    );
+
+                    end($value);
+                    $maxKey = key($value);
+
+
+                    $chart_width = max(count($super_array) * 5, 400);
+                    $options['width'] = $chart_width;
+                    $options['hAxis']['maxValue'] = $maxKey + 2;
+                    $options['hAxis']['gridlines']['count'] = ($maxKey + 2) / 2;
+
+                    echo '<h3>Games Played per Duration</h3>';
+                    echo '<div id="duration_breakdown" style="width: 400px;"></div>';
+
+                    echo '<div class="container">
                         <div class="col-sm-6">
                             <div class="table-responsive">
                                 <table class="table table-striped table-hover">';
-                        echo '<tr><th>Range</th><td>' . relative_time($modRange[0]['date_start']) . ' - ' . relative_time($modRange[0]['date_end']) . '</td></tr>';
-                        echo '<tr><th>Games Played</th><td>' . number_format(array_sum($testArray), 0) . '</td></tr>';
-                        echo '<tr><th>Combined Game Time</th><td>' . number_format($durationArray, 0) . ' mins</td></tr>';
-                        echo '</table>';
-                        echo '</div>
+                    echo '<tr><th>Range</th><td>' . relative_time($modRange[0]['date_start']) . ' - ' . relative_time($modRange[0]['date_end']) . '</td></tr>';
+                    echo '<tr><th>Games Played</th><td>' . number_format(array_sum($testArray), 0) . '</td></tr>';
+                    echo '<tr><th>Combined Game Time</th><td>' . number_format($durationArray, 0) . ' mins</td></tr>';
+                    echo '</table>';
+                    echo '</div>
                         </div>
                         </div>';
 
-                        $chart->load(json_encode($data));
-                        echo $chart->draw('duration_breakdown', $options);
+                    $chart->load(json_encode($data));
+                    echo $chart->draw('duration_breakdown', $options);
 
-                    }
                 } else {
                     echo 'No games played with that modID';
                 }
