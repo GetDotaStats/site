2f5a7f88d943f46033204f345856ea939f016402
jimmydorry <jimmydorry+github@gmail.com>
2014-11-06 16:20:44 +1000

    Fix UX issue caused by scrolling bar fix. Refresh will keep page stationary, while new pages will scroll up on load

--- getdotastats.js
+++ getdotastats.js
@@ -6,6 +6,12 @@ $(document).ready(function () {
         checkURL(this.hash);
     });
 
+    $(document).on("click", 'a.nav-refresh', function (e) {
+        checkURL2(this.hash);
+    });
+
+    //$('html, body').animate({ scrollTop: 0 }, 'fast');
+
     //setInterval("checkURL(window.location.hash)", 15 * 60 * 1000); //refresh every 15minutes
 });
 
@@ -23,7 +29,63 @@ function checkURL(hash) {
     }
 }
 
+function checkURL2(hash) {
+    if (!hash) {
+        loadPage2('#home');
+    }
+    else {
+        loadPage2(hash);
+    }
+}
+
 function loadPage(url) {
+    var oldURL = url;
+    url = url.replace('#', '').split('__').join('/');
+
+    if (url.indexOf('?') > -1 && url.indexOf('/?') < 0) {
+        url = url.replace('?', '.php?');
+    }
+    else if (url.slice(-1) != '/' && url.indexOf('?') < 0) {
+        url = url + '.php';
+    }
+
+    $('#loading').show({
+        start: function () {
+            $('#loading_spinner1').show();
+        },
+        complete: function () {
+            $.ajax({
+                type: "GET",
+                url: url,
+                dataType: "html",
+                success: function (msg) {
+                    document.getElementById("abcd").setAttribute("href", oldURL);
+                    setTimeout(function () {
+                        $('#loading').hide({
+                            complete: function () {
+                                if (parseInt(msg) != 0) {
+                                    $('#main_content').html(msg);
+                                }
+                                $('html, body').animate({ scrollTop: 0 }, 'fast');
+                            }
+                        });
+                    }, 500);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    $('#loading').hide({
+                        complete: function () {
+                            $('#main_content').html('Failed to load page. Try again later.');
+                            $('html, body').animate({ scrollTop: 0 }, 'fast');
+                        }
+                    });
+                }
+            });
+
+        }});
+
+}
+
+function loadPage2(url) {
     var oldURL = url;
     url = url.replace('#', '').split('__').join('/');
 
