0e2f8e3c13a2c2dca0309e45b53abfbf330dd384
jimmydorry <jimmydorry+github@gmail.com>
2014-11-13 13:02:27 +1000

    Add generic reporting for CSP incidents

---
+++ admin/csp_reports.php
@@ -0,0 +1,30 @@
+<?php
+try {
+    require_once('../global_functions.php');
+    require_once('../connections/parameters.php');
+
+    $db = new dbWrapper($hostname_gds_site, $username_gds_site, $password_gds_site, $database_gds_site, true);
+    if ($db) {
+        $reports = $db->q(
+            "SELECT * FROM `reports_csp` ORDER BY `reportDate` DESC;"
+        );
+
+        echo '<pre>';
+        foreach ($reports as $key => $value) {
+            echo relative_time($value['reportDate']) . "<br />";
+
+            echo json_encode(json_decode($value['reportContent'], 1), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+
+            echo '<br /><br />';
+
+            echo json_encode(json_decode($value['reportHeaders'], 1), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+
+            echo "<hr />";
+        }
+        echo '</pre>';
+    } else {
+        echo 'No DB!';
+    }
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
