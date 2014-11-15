1a206286012fc4d10df9e66172245000c44974f3
jimmydorry <your.name@domain>
2014-11-15 17:27:38 +1000

    Re-factor the JS loader

--- getdotastats.js
+++ getdotastats.js
@@ -1,23 +1,18 @@
 $(document).ready(function () {
-
-    checkURL(window.location.hash);
+    checkURL(window.location.hash, 1);
 
     $(document).on("click", 'a.nav-clickable', function (e) {
-        checkURL(this.hash);
+        checkURL(this.hash, 0);
     });
 
     $(document).on("click", 'a.nav-refresh', function (e) {
-        checkURL2(this.hash);
+        checkURL(this.hash, 1);
     });
-
-    //$('html, body').animate({ scrollTop: 0 }, 'fast');
-
-    //setInterval("checkURL(window.location.hash)", 15 * 60 * 1000); //refresh every 15minutes
 });
 
-function checkURL(hash) {
+function checkURL(hash, refresh) {
     if (!hash) {
-        loadPage('#home');
+        loadPage('#home', refresh);
     }
     else {
         var testElement = $('#navBarCustom');
@@ -25,20 +20,11 @@ function checkURL(hash) {
         testElement.find('.active').removeClass('active');
         testElement.find('a[href="' + hash + '"]').parents('li').addClass('active');
         //}
-        loadPage(hash);
+        loadPage(hash, refresh);
     }
 }
 
-function checkURL2(hash) {
-    if (!hash) {
-        loadPage2('#home');
-    }
-    else {
-        loadPage2(hash);
-    }
-}
-
-function loadPage(url) {
+function loadPage(url, refresh) {
     var oldURL = url;
     url = url.replace('#', '').replace('.', '').replace(':', '').split('__').join('/');
 
@@ -68,54 +54,8 @@ function loadPage(url) {
                                 if (parseInt(msg) != 0) {
                                     $('#main_content').html(msg);
                                 }
-                                $('html, body').animate({ scrollTop: 0 }, 'fast');
-                            }
-                        });
-                    }, 500);
-                },
-                error: function (jqXHR, textStatus, errorThrown) {
-                    $('#loading').hide({
-                        complete: function () {
-                            $('#main_content').html('Failed to load page. Try again later.');
-                            $('html, body').animate({ scrollTop: 0 }, 'fast');
-                        }
-                    });
-                }
-            });
-
-        }});
-
-}
-
-function loadPage2(url) {
-    var oldURL = url;
-    url = url.replace('#', '').replace('.', '').replace(':', '').split('__').join('/');
-
-    console.log("Loading: " + url);
-
-    if (url.indexOf('?') > -1 && url.indexOf('/?') < 0) {
-        url = url.replace('?', '.php?');
-    }
-    else if (url.slice(-1) != '/' && url.indexOf('?') < 0) {
-        url = url + '.php';
-    }
-
-    $('#loading').show({
-        start: function () {
-            $('#loading_spinner1').show();
-        },
-        complete: function () {
-            $.ajax({
-                type: "GET",
-                url: url,
-                dataType: "html",
-                success: function (msg) {
-                    document.getElementById("abcd").setAttribute("href", oldURL);
-                    setTimeout(function () {
-                        $('#loading').hide({
-                            complete: function () {
-                                if (parseInt(msg) != 0) {
-                                    $('#main_content').html(msg);
+                                if (!refresh) {
+                                    $('html, body').animate({ scrollTop: 0 }, 'fast');
                                 }
                             }
                         });
