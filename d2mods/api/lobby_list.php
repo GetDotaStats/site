3b570f6274288fd09c85b27b793fa05306c3fd48
jimmydorry <jimmydorry+github@gmail.com>
2015-01-14 09:56:09 +1000

    Add region filter to lobby list

--- d2mods/api/lobby_list.php
+++ d2mods/api/lobby_list.php
@@ -6,9 +6,13 @@ require_once('../../connections/parameters.php');
 //List of all the active lobbies on the site
 
 try {
+    $region = !empty($_GET['r']) && is_numeric($_GET['r'])
+        ? $_GET['r']
+        : 0;
+
     $memcache = new Memcache;
     $memcache->connect("localhost", 11211); # You might need to set "localhost" to "127.0.0.1"
-    $lobbyList = $memcache->get('api_d2mods_lobby_list');
+    $lobbyList = $memcache->get('api_d2mods_lobby_list' . $region);
     if (!$lobbyList) {
         $lobbyList = array();
 
@@ -16,30 +20,60 @@ try {
         $db->q('SET NAMES utf8;');
 
         if ($db) {
-            $lobbyListSQL = $db->q(
-                'SELECT
-                        ll.`lobby_id`,
-                        ll.`mod_id`,
-                        ll.`workshop_id`,
-                        ll.`lobby_name`,
-                        ll.`lobby_region`,
-                        ll.`lobby_max_players`,
-                        ll.`lobby_leader`,
-                        ll.`lobby_hosted`,
-                        ll.`lobby_pass`,
-                        ll.`lobby_map`,
-                        (
-                          SELECT
-                              COUNT(`user_id64`)
-                            FROM `lobby_list_players`
-                            WHERE `lobby_id` = ll.`lobby_id`
-                            LIMIT 0,1
-                        ) AS lobby_current_players
-                    FROM `lobby_list` ll
-                    WHERE ll.`lobby_active` = 1
-                    ORDER BY `lobby_id` DESC
-                    LIMIT 0,50;'
-            );
+            if (!empty($region)) {
+                $lobbyListSQL = $db->q(
+                    'SELECT
+                            ll.`lobby_id`,
+                            ll.`mod_id`,
+                            ll.`workshop_id`,
+                            ll.`lobby_name`,
+                            ll.`lobby_region`,
+                            ll.`lobby_max_players`,
+                            ll.`lobby_leader`,
+                            ll.`lobby_hosted`,
+                            ll.`lobby_pass`,
+                            ll.`lobby_map`,
+                            (
+                              SELECT
+                                  COUNT(`user_id64`)
+                                FROM `lobby_list_players`
+                                WHERE `lobby_id` = ll.`lobby_id`
+                                LIMIT 0,1
+                            ) AS lobby_current_players
+                        FROM `lobby_list` ll
+                        WHERE ll.`lobby_active` = 1 AND ll.`lobby_region` = ?
+                        ORDER BY `lobby_id` DESC
+                        LIMIT 0,50;',
+                    'i',
+                    $region
+                );
+            } else {
+                $lobbyListSQL = $db->q(
+                    'SELECT
+                            ll.`lobby_id`,
+                            ll.`mod_id`,
+                            ll.`workshop_id`,
+                            ll.`lobby_name`,
+                            ll.`lobby_region`,
+                            ll.`lobby_max_players`,
+                            ll.`lobby_leader`,
+                            ll.`lobby_hosted`,
+                            ll.`lobby_pass`,
+                            ll.`lobby_map`,
+                            (
+                              SELECT
+                                  COUNT(`user_id64`)
+                                FROM `lobby_list_players`
+                                WHERE `lobby_id` = ll.`lobby_id`
+                                LIMIT 0,1
+                            ) AS lobby_current_players
+                        FROM `lobby_list` ll
+                        WHERE ll.`lobby_active` = 1
+                        ORDER BY `lobby_id` DESC
+                        LIMIT 0,50;'
+                );
+            }
+
 
             if (!empty($lobbyListSQL)) {
                 foreach ($lobbyListSQL as $key => $value) {
