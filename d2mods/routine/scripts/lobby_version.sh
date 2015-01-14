9cf5805c4522f8345e16495b0066e770ea669b56
jimmydorry <jimmydorry+github@gmail.com>
2015-01-15 09:48:27 +1000

    BMD changed the version endpoint

--- d2mods/routine/scripts/lobby_version.sh
+++ d2mods/routine/scripts/lobby_version.sh
@@ -2,6 +2,6 @@
 cd /home/www-getdotastats/d2mods/api/
 echo "Run at: $(date -u)<br />"
 echo "-------------------<br />"
-wget --cache=off -O /home/www-getdotastats/d2mods/api/lobby_version.txt https://raw.githubusercontent.com/GetDotaStats/GetDotaLobby/lobbybrowser/vertest.txt
+wget --cache=off -O /home/www-getdotastats/d2mods/api/lobby_version.txt https://raw.githubusercontent.com/GetDotaStats/GetDotaLobby/lobbybrowser/version.txt
 echo "-------------------<br />"
 echo "Run at: $(date -u)"
