4164000e177f33df2ef4982b841034bfef07e618
jimmydorry <jimmydorry+github@gmail.com>
2015-01-15 08:45:00 +1000

    Add cronjob for version proxy

---
+++ d2mods/routine/scripts/lobby_version.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /home/www-getdotastats/d2mods/api/
+echo "Run at: $(date -u)<br />"
+echo "-------------------<br />"
+wget --cache=off -O /home/www-getdotastats/d2mods/api/lobby_version.txt https://raw.githubusercontent.com/GetDotaStats/GetDotaLobby/lobbybrowser/vertest.txt
+echo "-------------------<br />"
+echo "Run at: $(date -u)"
