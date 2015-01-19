cfc9c6020957275b46aaab6f4c2c64fcc11e5635
jimmydorry <jimmydorry+github@gmail.com>
2015-01-19 12:05:08 +1000

    Update lobby_keep_alive adding fields for lobby explorer

--- d2mods/api/lobby_keep_alive.php
+++ d2mods/api/lobby_keep_alive.php
@@ -18,21 +18,50 @@ try {
         $db = new dbWrapper_v2($hostname_gds_site, $username_gds_site, $password_gds_site, $database_gds_site, false);
         $db->q('SET NAMES utf8;');
 
-        $dateKeepAlive = date('Y-m-d G:i:s');
-
-        $sqlResult = $db->q(
-            'UPDATE `lobby_list` SET `date_keep_alive` = ? WHERE `lobby_id` = ? AND `lobby_active` = 1 AND `lobby_secure_token` = ?;',
-            'sis',
-            $dateKeepAlive, $lobbyID, $token
+        $lobbyDetails = $db->q(
+            'SELECT
+                    ll.`lobby_id`,
+                    ll.`mod_id`,
+                    ll.`workshop_id`,
+                    ll.`lobby_ttl`,
+                    ll.`lobby_active`,
+                    ll.`date_keep_alive`,
+                    ll.`date_recorded`
+                FROM `lobby_list` ll
+                WHERE ll.`lobby_id` = ? AND ll.`lobby_active` = 1
+                ORDER BY `lobby_id` DESC
+                LIMIT 0,1;',
+            'i',
+            $lobbyID
         );
 
-        if (!empty($sqlResult)) {
-            //RETURN LOBBY ID
-            $lobbyStatus['result'] = 'Lobby ' . $lobbyID . ' kept alive!';
-            $lobbyStatus['token'] = $token;
-        } else {
-            //SOMETHING FUNKY HAPPENED
-            $lobbyStatus['error'] = 'Lobby not updated! Either already updated or invalid fields.';
+        if (!empty($lobbyDetails)) {
+            $lobbyDetails = $lobbyDetails[0];
+
+            $dateKeepAlive = date('Y-m-d G:i:s');
+
+            $sqlResult = $db->q(
+                'UPDATE `lobby_list` SET `date_keep_alive` = ? WHERE `lobby_id` = ? AND `lobby_active` = 1 AND `lobby_secure_token` = ?;',
+                'sis',
+                $dateKeepAlive, $lobbyID, $token
+            );
+
+            if (!empty($sqlResult)) {
+                //RETURN LOBBY ID
+                $lobbyStatus['result'] = 'Lobby ' . $lobbyID . ' kept alive!';
+                $lobbyStatus['token'] = $token;
+                $lobbyStatus['lobby_ttl'] = $lobbyDetails['lobby_ttl'];
+                $lobbyStatus['lobby_active'] = $lobbyDetails['lobby_active'];
+                $lobbyStatus['date_keep_alive'] = relative_time($dateKeepAlive);
+                $lobbyStatus['date_recorded'] = relative_time($lobbyDetails['date_recorded']);
+            } else {
+                //SOMETHING FUNKY HAPPENED
+                $lobbyStatus['error'] = 'Lobby not updated! Either already updated or invalid fields.';
+            }
+        }
+        else{
+            $lobbyStatus['lobby_active'] = 0;
+            $lobbyStatus['error'] = 'Lobby does not exist or is in-active.';
         }
     } else {
         $lobbyStatus['error'] = 'Missing field!';
