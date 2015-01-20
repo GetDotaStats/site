0f1a7a0a0bd6ff4374580d0294285b824f63a8b4
jimmydorry <jimmydorry+github@gmail.com>
2015-01-21 09:15:47 +1000

    Update lobby_list to show recently closed games

--- d2mods/lobby_list.php
+++ d2mods/lobby_list.php
@@ -61,7 +61,7 @@ try {
                     ) AS lobby_current_players
                 FROM `lobby_list` ll
                 LEFT JOIN `mod_list` ml ON ll.`mod_id` = ml.`mod_id`
-                WHERE ll.`lobby_active` = 0 AND ll.`lobby_hosted` = 1
+                WHERE ll.`lobby_active` = 0
                 ORDER BY ll.`date_recorded` DESC
                 LIMIT 0,20;'
             , 5
