35b13d90f4183c97a893f5ba98dcbcea84737643
jimmydorry <jimmydorry+github@gmail.com>
2015-01-22 13:47:24 +1000

    Lock down test page

--- d2mods/test/update_valfs.php
+++ d2mods/test/update_valfs.php
@@ -4,41 +4,51 @@ require_once('../functions.php');
 require_once('../../connections/parameters.php');
 
 try {
-    $db = new dbWrapper_v2($hostname_gds_site, $username_gds_site, $password_gds_site, $database_gds_site);
+    if (!isset($_SESSION)) {
+        session_start();
+    }
 
-    if ($db) {
-        $valfs = $db->q("SELECT * FROM `steam_valfs`;");
+    checkLogin_v2();
 
-        $webAPI = new steam_webapi($api_key1);
+    if (!empty($_SESSION['user_id64']) && !empty($_SESSION['isAdmin'])) {
+        $db = new dbWrapper_v2($hostname_gds_site, $username_gds_site, $password_gds_site, $database_gds_site);
 
-        if (!empty($valfs)) {
-            foreach ($valfs as $key => $value) {
-                $userID64 = $value['user_id64'];
+        if ($db) {
+            $valfs = $db->q("SELECT * FROM `steam_valfs`;");
 
-                $valfUser = new SteamID($userID64);
-                $userID32 = $valfUser->getSteamID32();
+            $webAPI = new steam_webapi($api_key1);
 
-                $profileRequest = $webAPI->GetPlayerSummariesV2($userID64);
+            if (!empty($valfs)) {
+                foreach ($valfs as $key => $value) {
+                    $userID64 = $value['user_id64'];
 
-                $userName = !empty($profileRequest)
-                    ? $profileRequest['response']['players'][0]['personaname']
-                    : '??';
+                    $valfUser = new SteamID($userID64);
+                    $userID32 = $valfUser->getSteamID32();
 
-                echo $userID64 . ' | ' . $userName . '<br />';
+                    $profileRequest = $webAPI->GetPlayerSummariesV2($userID64);
 
-                $db->q(
-                    'INSERT INTO `steam_valfs` (`user_id64`, `user_id32`, `user_name`)
-                        VALUES (?, ?, ?)
-                        ON DUPLICATE KEY UPDATE `user_id32` = VALUES(`user_id32`), `user_name` = VALUES(`user_name`);',
-                    'sss',
-                    $userID64, $userID32, $userName
-                );
+                    $userName = !empty($profileRequest)
+                        ? $profileRequest['response']['players'][0]['personaname']
+                        : '??';
+
+                    echo $userID64 . ' | ' . $userName . '<br />';
+
+                    $db->q(
+                        'INSERT INTO `steam_valfs` (`user_id64`, `user_id32`, `user_name`)
+                            VALUES (?, ?, ?)
+                            ON DUPLICATE KEY UPDATE `user_id32` = VALUES(`user_id32`), `user_name` = VALUES(`user_name`);',
+                        'sss',
+                        $userID64, $userID32, $userName
+                    );
+                }
+            } else {
+                echo bootstrapMessage('Oh Snap', 'No known Valfs!', 'dangerk');
             }
         } else {
-            echo bootstrapMessage('Oh Snap', 'No known Valfs!', 'dangerk');
+            echo bootstrapMessage('Oh Snap', 'No DB!', 'danger');
         }
     } else {
-        echo bootstrapMessage('Oh Snap', 'No DB!', 'danger');
+        echo bootstrapMessage('Oh Snap', 'Not logged in!', 'danger');
     }
 } catch (Exception $e) {
     $message = 'Caught Exception -- ' . $e->getFile() . ':' . $e->getLine() . '<br /><br />' . $e->getMessage();
