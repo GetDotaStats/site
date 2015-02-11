a28cf104f73bdb3ad3ab983dafb5947a3869c9a2
jimmydorry <jimmydorry+github@gmail.com>
2015-02-11 16:46:55 +1000

    Fix steamcommunity avatar new domain

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ try {
           connect-src 'self' static.getdotastats.com getdotastats.com;
           style-src 'self' static.getdotastats.com getdotastats.com 'unsafe-inline' ajax.googleapis.com *.google.com;
           script-src 'self' static.getdotastats.com getdotastats.com oss.maxcdn.com ajax.googleapis.com *.google.com 'unsafe-eval' 'unsafe-inline';
-          img-src 'self' dota2.photography static.getdotastats.com getdotastats.com media.steampowered.com data: ajax.googleapis.com cdn.akamai.steamstatic.com cdn.dota2.com *.gstatic.com steamcommunity-a.akamaihd.net;
+          img-src 'self' dota2.photography static.getdotastats.com getdotastats.com media.steampowered.com data: ajax.googleapis.com cdn.akamai.steamstatic.com cdn.dota2.com *.gstatic.com *.akamaihd.net;
           font-src 'self' static.getdotastats.com getdotastats.com;
           frame-src chatwing.com *.youtube.com;
           object-src 'none';
