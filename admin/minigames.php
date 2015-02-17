7740b43178342454a5e486775b3bdd34297fbae3
jimmydorry <jimmydorry+github@gmail.com>
2015-02-17 12:03:12 +1000

    Update minigame creation form, compacting it

--- admin/minigames.php
+++ admin/minigames.php
@@ -24,97 +24,83 @@ try {
     echo '<h2>Create a Mini Game <small>BETA</small></h2>';
     echo '<p>This form allows admins to create Mini Game entries.</p>';
 
-    echo '<div class="container"><div class="col-sm-6">';
     echo '<form id="minigameCreate">';
-    echo '
-                        <div class="table-responsive">
-                            <table class="table table-striped table-hover">
-                                <tr>
-                                    <th>Mini Game Name</th>
-                                    <td>
-                                        <span class="glyphicon glyphicon-question-sign" title="The name of your mini-game"></span>
-                                    </td>
-                                    <td>
-                                        <input name="minigame_name" type="text" maxlength="70" size="45" placeholder="Super Awesome Fun Time" required>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th>Developer</th>
-                                    <td>
-                                        <span class="glyphicon glyphicon-question-sign" title="The userID of the developer of the mini game. Can be the community URL, userID64 or userID32"></span>
-                                    </td>
-                                    <td>
-                                        <input name="minigame_developer" type="text" maxlength="70" size="45" placeholder="http://steamcommunity.com/id/jimmydorry/" required>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th>Steam Group</th>
-                                    <td>
-                                        <span class="glyphicon glyphicon-question-sign" title="(OPTIONAL) The full link to your game group, should you wish to create a community around your minigame"></span>
-                                    </td>
-                                    <td>
-                                        <input name="minigame_steam_group" type="text" maxlength="70" size="45" placeholder="http://steamcommunity.com/groups/XXXXX">
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th>Description</th>
-                                    <td>
-                                        <span class="glyphicon glyphicon-question-sign" title="A description of the mini-game to be added"></span>
-                                    </td>
-                                    <td>
-                                        <textarea name="minigame_description" cols="46" placeholder="A fun game where you do stuff" required></textarea>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td colspan="3">&nbsp;</td>
-                                </tr>
-                                <tr>
-                                    <th>Score Objective</th>
-                                    <td>
-                                        <span class="glyphicon glyphicon-question-sign" title="Are users to trying to maximise or minimise their score?"></span>
-                                    </td>
-                                    <td>
-                                        <input type="radio" name="minigame_objective" value="max" checked>Maximise<br />
-                                        <input type="radio" name="minigame_objective" value="min">Minimise<br />
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th>Score Operator</th>
-                                    <td>
-                                        <span class="glyphicon glyphicon-question-sign" title="How do we handle the score?"></span>
-                                    </td>
-                                    <td>
-                                        <input type="radio" name="minigame_operator" value="multiply" checked>Multiply<br />
-                                        <input type="radio" name="minigame_operator" value="divide">Divide
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th>Score Factor</th>
-                                    <td>
-                                        <span class="glyphicon glyphicon-question-sign" title="What factor are we using on the score?"></span>
-                                    </td>
-                                    <td>
-                                        <input type="number" name="minigame_factor" maxlength="20" size="4" value="1" min="0" max="1000" required>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th>Score Decimals</th>
-                                    <td>
-                                        <span class="glyphicon glyphicon-question-sign" title="How many decimal points do we want on the leaderboard?"></span>
-                                    </td>
-                                    <td>
-                                        <input type="number" name="minigame_decimals" maxlength="20" size="4" value="2" min="0" max="10" required>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td colspan="3" align="center">
-                                        <button id="sub">Create</button>
-                                    </td>
-                                </tr>
-                            </table>
-                        </div>';
+
+    echo '<div class="row">
+                <div class="col-md-1"><span class="h4">Name</span></div>
+                <div class="col-md-1 text-center"><span class="glyphicon glyphicon-question-sign" title="The name of your mini-game"></span></div>
+                <div class="col-md-6"><input class="formTextArea boxsizingBorder"  name="minigame_name" type="text" maxlength="70" size="45" placeholder="Super Awesome Fun Time" required></div>
+
+                <div class="col-md-1"><strong>Objective</strong></div>
+                <div class="col-md-1 text-center">
+                    <span class="glyphicon glyphicon-question-sign" title="Are users to trying to maximise or minimise their score?"></span>
+                </div>
+                <div class="col-md-2">
+                    <input type="radio" name="minigame_objective" value="max" checked>Maximise<br />
+                    <input type="radio" name="minigame_objective" value="min">Minimise<br />
+                </div>
+            </div>';
+
+    echo '<span class="h5">&nbsp;</span>';
+
+    echo '<div class="row">
+                <div class="col-md-1"><span class="h4">Developer</span></div>
+                <div class="col-md-1 text-center"><span class="glyphicon glyphicon-question-sign" title="The userID of the developer of the mini game. Can be the community URL, userID64 or userID32"></span></div>
+                <div class="col-md-6"><input class="formTextArea boxsizingBorder"  name="minigame_developer" type="text" maxlength="70" size="45" placeholder="http://steamcommunity.com/id/jimmydorry/" required></div>
+
+                <div class="col-md-1"><strong>Operator</strong></div>
+                <div class="col-md-1 text-center">
+                    <span class="glyphicon glyphicon-question-sign" title="How do we handle the score?"></span>
+                </div>
+                <div class="col-md-2">
+                    <input type="radio" name="minigame_operator" value="multiply" checked>Multiply<br />
+                    <input type="radio" name="minigame_operator" value="divide">Divide
+                </div>
+            </div>';
+
+    echo '<span class="h5">&nbsp;</span>';
+
+    echo '<div class="row">
+                <div class="col-md-1"><span class="h4">Group</span></div>
+                <div class="col-md-1 text-center"><span class="glyphicon glyphicon-question-sign" title="(OPTIONAL) The full link to your game group, should you wish to create a community around your minigame"></span></div>
+                <div class="col-md-6"><input class="formTextArea boxsizingBorder" name="minigame_steam_group" type="text" maxlength="70" placeholder="http://steamcommunity.com/groups/XXXXX"></div>
+
+                <div class="col-md-1"><strong>Factor</strong></div>
+                <div class="col-md-1 text-center">
+                    <span class="glyphicon glyphicon-question-sign" title="What factor are we using on the score?"></span>
+                </div>
+                <div class="col-md-2">
+                    <input class="formTextArea boxsizingBorder" type="number" name="minigame_factor" maxlength="20" value="1" min="0" max="1000" required>
+                </div>
+            </div>';
+
+    echo '<span class="h5">&nbsp;</span>';
+
+    echo '<div class="row">
+                <div class="col-md-1"><span class="h4">Desc.</span></div>
+                <div class="col-md-1 text-center"><span class="glyphicon glyphicon-question-sign" title="A description of the mini-game to be added"></span></div>
+                <div class="col-md-6"><textarea class="formTextArea boxsizingBorder" name="minigame_description" placeholder="A fun game where you do stuff" required></textarea></div>
+
+                <div class="col-md-1"><strong>Decimals</strong></div>
+                <div class="col-md-1 text-center">
+                    <span class="glyphicon glyphicon-question-sign" title="How many decimal points do we want on the leaderboard?"></span>
+                </div>
+                <div class="col-md-2">
+                    <input class="formTextArea boxsizingBorder" type="number" name="minigame_decimals" maxlength="20" value="2" min="0" max="10" required>
+                </div>
+            </div>';
+
+    echo '<span class="h5">&nbsp;</span>';
+
+    echo '<div class="row">
+                <div class="col-md-12 text-center">
+                    <button id="sub">Create</button>
+                </div>
+            </div>';
+
+    echo '<span class="h5">&nbsp;</span>';
+
     echo '</form>';
-    echo '</div></div>';
 
     echo '<span id="minigameAJAXResult" class="labelWarnings label label-danger"></span>';
 
@@ -194,32 +180,32 @@ try {
             : 'No Group';
 
         $mgObjective = !empty($value['minigameObjective']) && $value['minigameObjective'] == 'max'
-            ? '<select name="minigameObjective">
+            ? '<select name="minigameObjective" class="formTextArea boxsizingBorder">
                         <option value="min">Minimise</option>
                         <option value="max" selected>Maximise</option>
                     </select>'
-            : '<select name="minigameObjective">
+            : '<select name="minigameObjective" class="formTextArea boxsizingBorder">
                         <option value="min" selected>Minimise</option>
                         <option value="max">Maximise</option>
                     </select>';
 
         $mgOperator = !empty($value['minigameOperator']) && $value['minigameOperator'] == 'divide'
-            ? '<select name="minigameOperator">
+            ? '<select name="minigameOperator" class="formTextArea boxsizingBorder">
                         <option value="multiply">Multiply</option>
                         <option value="divide" selected>Divide</option>
                     </select>'
-            : '<select name="minigameOperator">
+            : '<select name="minigameOperator" class="formTextArea boxsizingBorder">
                         <option value="multiply" selected>Multiply</option>
                         <option value="divide">Divide</option>
                     </select>';
 
         $mgFactor = !empty($value['minigameFactor']) && is_numeric($value['minigameFactor'])
-            ? '<input type="number" name="minigameFactor" maxlength="20" size="4" value="' . floatval($value['minigameFactor']) . '" min="0" max="1000">'
-            : '<input type="number" name="minigameFactor" maxlength="20" size="4" value="1" min="0" max="1000">';
+            ? '<input class="formTextArea boxsizingBorder" type="number" name="minigameFactor" maxlength="20" size="4" value="' . floatval($value['minigameFactor']) . '" min="0" max="1000">'
+            : '<input class="formTextArea boxsizingBorder" type="number" name="minigameFactor" maxlength="20" size="4" value="1" min="0" max="1000">';
 
         $mgDecimals = !empty($value['minigameDecimals']) && is_numeric($value['minigameDecimals'])
-            ? '<input type="number" name="minigameDecimals" maxlength="20" size="4" value="' . $value['minigameDecimals'] . '" min="0" max="10">'
-            : '<input type="number" name="minigameDecimals" maxlength="20" size="4" value="2" min="0" max="10">';
+            ? '<input class="formTextArea boxsizingBorder" type="number" name="minigameDecimals" maxlength="20" size="4" value="' . $value['minigameDecimals'] . '" min="0" max="10">'
+            : '<input class="formTextArea boxsizingBorder" type="number" name="minigameDecimals" maxlength="20" size="4" value="2" min="0" max="10">';
 
         echo '<div class="row">
                 <div class="col-md-6">
@@ -246,21 +232,21 @@ try {
         echo '<div class="row">
                     <div class="col-md-1">&nbsp;</div>
                     <div class="col-md-1"><strong>Objective</strong></div>
-                    <div class="col-md-1">' . $mgObjective . '</div>
+                    <div class="col-md-2">' . $mgObjective . '</div>
 
-                    <div class="col-md-1">&nbsp;</div>
                     <div class="col-md-1"><strong>Operator</strong></div>
-                    <div class="col-md-1">' . $mgOperator . '</div>
+                    <div class="col-md-2">' . $mgOperator . '</div>
                 </div>';
+
+        echo '<span class="h5">&nbsp;</span>';
 
         echo '<div class="row">
                     <div class="col-md-1">&nbsp;</div>
                     <div class="col-md-1"><strong>Factor</strong></div>
-                    <div class="col-md-1">' . $mgFactor . '</div>
+                    <div class="col-md-2">' . $mgFactor . '</div>
 
-                    <div class="col-md-1">&nbsp;</div>
                     <div class="col-md-1"><strong>Decimals</strong></div>
-                    <div class="col-md-1">' . $mgDecimals . '</div>
+                    <div class="col-md-2">' . $mgDecimals . '</div>
                 </div>';
 
         echo '<span class="h5">&nbsp;</span>';
