7fa133a13a406fec419476a2dd0caa9f43982344
jimmydorry <jimmydorry+github@gmail.com>
2015-03-04 13:59:28 +1000

    Add MMR tracking end point

--- d2mods/api/stat_user.php
+++ d2mods/api/stat_user.php
@@ -48,27 +48,74 @@ try {
     $db = new dbWrapper_v3($hostname_gds_site, $username_gds_site, $password_gds_site, $database_gds_site, true);
     if (empty($db)) throw new Exception('No DB!');
 
-    $userID = new SteamID($userID);
+    $memcache = new Memcache;
+    $memcache->connect("localhost", 11211); # You might need to set "localhost" to "127.0.0.1"
 
-    $lobbyStatus['userID'] = $userID;
+    $steamID = new SteamID($userID);
+
+    $lobbyStatus['userID32'] = $steamID->getsteamID32();
+    $lobbyStatus['userID64'] = $steamID->getsteamID64();
     $lobbyStatus['username'] = $username;
     $lobbyStatus['totalGames'] = $totalGames;
     $lobbyStatus['soloMMR'] = $soloMMR;
     $lobbyStatus['teamMMR'] = $teamMMR;
     $lobbyStatus['statsDisabled'] = $statsDisabled;
 
-    $sqlResult = $db->q(
-        'INSERT INTO `gds_users_mmr`(`user_id32`, `user_id64`, `user_name`, `user_games`, `user_mmr_solo`, `user_mmr_party`, `user_stats_disabled`, `date_recorded`)
-            VALUES (?, ?, ?, ?, ?, ?, ?, NULL);',
-        'sssiiii',
-        $userID->getSteamID32(), $userID->getSteamID64(), $username, $totalGames, $soloMMR, $teamMMR, $statsDisabled
+    $previousMMR = cached_query(
+        'api_user_mmr_previous' . $userID,
+        'SELECT
+                `user_id32`,
+                `user_id64`,
+                `user_name`,
+                `user_games`,
+                `user_mmr_solo`,
+                `user_mmr_party`,
+                `user_stats_disabled`,
+                `date_recorded`
+            FROM `gds_users_mmr`
+            WHERE `user_id64` = ?
+            ORDER BY `date_recorded` DESC
+            LIMIT 0,1;',
+        's',
+        array($steamID->getSteamID64()),
+        5
     );
 
-    if (!empty($sqlResult)) {
-        $lobbyStatus['result'] = 'Results recorded for ' . $db->last_index() . ' created!';
-    } else {
-        $lobbyStatus['error'] = 'Unknown error!';
+    $MMRcheck = true;
+    if (!empty($previousMMR)) {
+        //$lastRecord = relative_time_v3($previousMMR[0]['date_recorded'], 1, 'day', true);
+        //$lastRecord = $lastRecord['number'];
+
+        $previousRecord = new DateTime($previousMMR[0]['date_recorded']);
+        $todayRecord = date('Y-m-d');
+
+        if ($soloMMR > ($previousMMR[0]['user_mmr_solo'] + 50) && $totalGames <= ($previousMMR[0]['user_games'] + 1)) throw new Exception('Anti-Cheat 1 triggered!');
+
+        if ($teamMMR > ($previousMMR[0]['user_mmr_party'] + 50) && $totalGames <= ($previousMMR[0]['user_games'] + 1)) throw new Exception('Anti-Cheat 2 triggered!');
+
+        if ($totalGames <= ($previousMMR[0]['user_games'] - 5)) throw new Exception('Anti-Cheat 3 triggered!');
+
+        if ($teamMMR == $previousMMR[0]['user_mmr_party'] && $soloMMR == $previousMMR[0]['user_mmr_solo'] && ($previousRecord->format('Y-m-d') == $todayRecord)) $MMRcheck = false;
     }
+
+    if ($MMRcheck) {
+        $sqlResult = $db->q(
+            'INSERT INTO `gds_users_mmr`(`user_id32`, `user_id64`, `user_name`, `user_games`, `user_mmr_solo`, `user_mmr_party`, `user_stats_disabled`, `date_recorded`)
+                VALUES (?, ?, ?, ?, ?, ?, ?, NULL);',
+            'sssiiii',
+            $steamID->getSteamID32(), $steamID->getSteamID64(), $username, $totalGames, $soloMMR, $teamMMR, $statsDisabled
+        );
+
+        if (!empty($sqlResult)) {
+            $lobbyStatus['result'] = 'MMR updated!';
+        } else {
+            $lobbyStatus['error'] = 'Unknown error!';
+        }
+    } else {
+        $lobbyStatus['result'] = 'MMR not recorded due to lack of change!';
+    }
+
+    $memcache->close();
 } catch (Exception $e) {
     unset($lobbyStatus);
     $lobbyStatus['error'] = 'Contact getdotastats.com - Caught Exception: ' . $e->getMessage();
