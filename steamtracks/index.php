cd64aab1e2f6defa008ebd0a12752228bd10c90f
jimmydorry <jimmydorry+github@gmail.com>
2015-03-06 17:42:14 +1000

    Update signature description

--- steamtracks/index.php
+++ steamtracks/index.php
@@ -1,4 +1,5 @@
 <?php
+require_once('../global_functions.php');
 require_once('./functions.php');
 require_once('../connections/parameters.php');
 
@@ -7,62 +8,29 @@ try {
         session_start();
     }
 
-    $db = new dbWrapper($hostname_steamtracks, $username_steamtracks, $password_steamtracks, $database_steamtracks, false);
-    $steamtracks = new steamtracks($steamtracks_api_key, $steamtracks_api_secret, false);
+    $db = new dbWrapper_v3($hostname_steamtracks, $username_steamtracks, $password_steamtracks, $database_steamtracks, false);
 
     if (!empty($_SESSION['user_id64'])) {
         $steamid64 = $_SESSION['user_id64'];
         $steamid32 = $_SESSION['user_id32'];
     }
 
-    if (!empty($_GET['status']) && $_GET['status'] == 'success' && !empty($steamid32)) {
-        $file_name_location = '../sig/images/generated/' . $steamid32 . '.png';
-
-        if (file_exists($file_name_location)) {
-            @unlink($file_name_location);
-        }
-    }
-
     echo '<h3>Signature Generator</h3>';
 
     echo '<span class="h4">&nbsp;</span>';
 
-    echo '<div class="panel panel-danger">
+    echo '<div class="panel panel-warning">
             <div class="panel-heading">
-                <h3 class="panel-title">Warning!</h3>
+                <h3 class="panel-title">Update!</h3>
             </div>
-            <div class="panel-body">SteamTracks will be losing functionality soon. For users that wish to retain
-                MMR tracking functionality, please install our Lobby Explorer and opt-in to MMR tracking via the main menu. In the
-                coming days we will start using the MMR we collect via the Lobby Explorer, instead of the MMR we get via SteamTracks.
+            <div class="panel-body">We have just rolled out the new MMR tracking system. The MMR on your signature is now
+            obtained via our Lobby Explorer. Make sure you are running the latest version, and have opted into MMR sharing.
+            Be aware that for the first 2hours of its release, you may need to hard refresh your signature (CTRL + R) to see it updated.
+            Report issues to our <a target="_blank" href="http://github.com/GetDotaStats/site/issues">Issue Tracker</a>.
             </div>
         </div>';
 
     echo '<span class="h4">&nbsp;</span>';
-
-    if (isset($_GET['status'])) {
-        echo '<div class="alert alert-danger">';
-        switch ($_GET['status']) {
-            case 'success':
-                echo 'Sucessfully enrolled as new user!';
-                break;
-            case 'sqlfailure':
-                echo 'Could not insert your stats into database. This means that we may already have stats for you. If you can see the app listed under your steamtracks apps list, and you have the bot added, then we will automatically grab your stats later.';
-                break;
-            case 'apifailure':
-                echo 'Failure receiving account stats. If you signed up correctly, we will retry grabbing your stats automatically at a later date.';
-                break;
-            case 'missingidtoken':
-                echo 'Missing steam_id or token. <a class="nav-clickable" href="#steamtracks/?status=apifailure">Please try again.</a>';
-                break;
-            case 'sidfailure':
-                echo 'Bad Steam ID given via SteamTracks. Report this error in the chatbox or github';
-                break;
-            case 'readd':
-                echo 'Re-doing SteamTrack authentication';
-                break;
-        }
-        echo '</div><br />';
-    }
 
     if (empty($steamid32)) {
         echo 'To get your own Dota2 signature, login via steam at the top right of the screen. Logging in does not grant us access to your private data. After logging in, you will be presented with your signature and also have the option of adding your MMR to your signature via SteamTracks OAuth.<br /><br />';
@@ -70,31 +38,15 @@ try {
         echo '<img src="http://getdotastats.com/sig/' . $steamid32 . '.png" /><br />';
         echo '<strong>Your signature link:</strong> <a target="__new" href="http://getdotastats.com/sig/' . $steamid32 . '.png">http://getdotastats.com/sig/' . $steamid32 . '.png</a><br /><br />';
 
-        echo 'Signatures are cached for up to 2hours. MMR stats are updated every 12hours. As long as you have the bot added, your stats will eventually update.<br /><br />';
-
-        echo '<h3>Adding MMR to your sig:</h3>';
-        echo '<p>There may be issues, as there are rumours that SteamTracks will stop providing this service soon. ';
-        echo 'Issues should be reported <a href="https://github.com/bontscho/steamtracks/issues" target="_blank">here</a>. ';
-        echo 'Let us know via the chatbox on the right if you have issues or questions.</p>';
-
-        $gotDBstats = $db->q(
-            'SELECT * FROM `mmr` WHERE `steam_id` = ? LIMIT 0,1;',
-            'i',
-            $steamid32
-        );
-
-        if (((!isset($_GET['status']) && empty($gotDBstats)) || (isset($_GET['status']) && $_GET['status'] == 'readd')) && !empty($steamid32)) {
-            $token_response = $steamtracks->signup_token($steamid32, 'true'); //GET TOKEN
-
-            if (!empty($token_response['result']['token'])) {
-                $token = $token_response['result']['token'];
-                echo '<br /><br /><a href="https://steamtracks.com/appauth/' . $token . '">CLICK HERE TO ADD MRR FOR THE ABOVE SIGNATURE</a><br /><br />';
-            } else {
-                var_dump($token_response);
-            }
-        } else if (!empty($gotDBstats)) {
-            echo 'We already have stats for you. If you removed yourself from the app, you can <a class="nav-clickable" href="#steamtracks/?status=readd">re-add yourself here</a>.<br />';
-        }
+        echo '<div class="panel panel-danger">
+            <div class="panel-heading">
+                <h3 class="panel-title">Note</h3>
+            </div>
+            <div class="panel-body">Signatures are cached for 2 hours. MMR is updated in our back-end every time you open the
+            Dota2 client (if the MMR has changed since the last report), but not updated on the signature until it expires. Your username
+            and avatar only update when you login to the site again. We will make it easier to expire your signature in the near future.
+            </div>
+        </div>';
     }
 } catch (Exception $e) {
     echo $e->getMessage();
