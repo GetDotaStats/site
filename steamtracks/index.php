bf2c3b2b4e4242cca913f7f5916dd1d4f608458d
jimmydorry <jimmydorry+github@gmail.com>
2015-03-10 15:54:59 +1000

    Update signature generator to allow for sig regen that also grabs the changed username/avatar

--- steamtracks/index.php
+++ steamtracks/index.php
@@ -8,12 +8,17 @@ try {
         session_start();
     }
 
-    $db = new dbWrapper_v3($hostname_steamtracks, $username_steamtracks, $password_steamtracks, $database_steamtracks, false);
+    $db = new dbWrapper_v3($hostname_gds_site, $username_gds_site, $password_gds_site, $database_gds_site, true);
+    if (empty($db)) throw new Exception('No DB!');
 
-    if (!empty($_SESSION['user_id64'])) {
-        $steamid64 = $_SESSION['user_id64'];
-        $steamid32 = $_SESSION['user_id32'];
-    }
+    $memcache = new Memcache;
+    $memcache->connect("localhost", 11211); # You might need to set "localhost" to "127.0.0.1"
+
+    checkLogin_v2();
+    if (empty($_SESSION['user_id64'])) throw new Exception('Not logged in!');
+
+    $steamID = new SteamID($_SESSION['user_id64']);
+    if (empty($steamID->getSteamID32()) || empty($steamID->getSteamID64())) throw new Exception('Bad steamID!');
 
     echo '<h3>Signature Generator</h3>';
 
@@ -25,29 +30,45 @@ try {
             </div>
             <div class="panel-body">We have just rolled out the new MMR tracking system. The MMR on your signature is now
             obtained via our Lobby Explorer. Make sure you are running the latest version, and have opted into MMR sharing.
-            Be aware that for the first 2hours of its release, you may need to hard refresh your signature (CTRL + R) to see it updated.
             Report issues to our <a target="_blank" href="http://github.com/GetDotaStats/site/issues">Issue Tracker</a>.
             </div>
         </div>';
 
     echo '<span class="h4">&nbsp;</span>';
 
-    if (empty($steamid32)) {
+    if (empty($steamID->getSteamID32())) {
         echo 'To get your own Dota2 signature, login via steam at the top right of the screen. Logging in does not grant us access to your private data. After logging in, you will be presented with your signature and also have the option of adding your MMR to your signature via SteamTracks OAuth.<br /><br />';
     } else {
-        echo '<img src="http://getdotastats.com/sig/' . $steamid32 . '.png" /><br />';
-        echo '<strong>Your signature link:</strong> <a target="__new" href="http://getdotastats.com/sig/' . $steamid32 . '.png">http://getdotastats.com/sig/' . $steamid32 . '.png</a><br /><br />';
+        if (isset($_GET['refresh'])) {
+            updateUserDetails($steamID->getSteamID64(), $api_key1);
+
+            $file_name_location = '../sig/images/generated/' . $steamID->getsteamID32() . '_main.png';
+            if (file_exists($file_name_location)) {
+                @unlink($file_name_location);
+            }
+        }
+
+        echo '<div class="row">
+                <div class="text-center">
+                    <img src="http://getdotastats.com/sig/' . $steamID->getSteamID32() . '.png" /><br />
+                    <p><strong>Your signature link:</strong> <a target="__new" href="http://getdotastats.com/sig/' . $steamID->getSteamID32() . '.png">http://getdotastats.com/sig/' . $steamID->getSteamID32() . '.png</a></p>
+                    <p><a class="nav-clickable btn btn-danger btn-lg" href="#steamtracks/?refresh">Refresh Signature</a></p>
+                </div>
+            </div>';
 
         echo '<div class="panel panel-danger">
             <div class="panel-heading">
                 <h3 class="panel-title">Note</h3>
             </div>
-            <div class="panel-body">Signatures are cached for 2 hours. MMR is updated in our back-end every time you open the
-            Dota2 client (if the MMR has changed since the last report), but not updated on the signature until it expires. Your username
-            and avatar only update when you login to the site again. We will make it easier to expire your signature in the near future.
+            <div class="panel-body">Signatures are cached for 2 hours in your browser. After refreshing your signature, you will
+            need hard refresh (CTRL + R) on the image link to expire it in your browser. This will not work in all browsers.
+            MMR is updated every time you open the Dota2 client. Your username and avatar only update when you login to the
+            site again, or click the Refresh button above.
             </div>
         </div>';
     }
+
+    $memcache->close();
 } catch (Exception $e) {
     echo $e->getMessage();
 }
