bc07106b7a028bf24b2872ee4a14a40a357bf333
jimmydorry <jimmydorry+github@gmail.com>
2015-09-07 12:14:28 +1000

    Update connections sample

--- connections/parametersSAMPLE.php
+++ connections/parametersSAMPLE.php
@@ -3,52 +3,87 @@
 $hostname_match_analysis = "localhost";
 $database_match_analysis = "DB_NAME";
 $username_match_analysis = "DB_USERNAME";
-$password_match_analysis = "DB_PASS";
+$password_match_analysis = "DB_PASSWORD";
 
 //MATCH ANALYSIS CRONJOBS
 $hostname_match_analysis_routine = "localhost";
 $database_match_analysis_routine = "DB_NAME";
 $username_match_analysis_routine = "DB_USERNAME";
-$password_match_analysis_routine = "DB_PASS";
+$password_match_analysis_routine = "DB_PASSWORD";
 
 //SIGNATURE GENERATOR
 $hostname_sig = "localhost";
 $database_sig = "DB_NAME";
 $username_sig = "DB_USERNAME";
-$password_sig = "DB_PASS";
+$password_sig = "DB_PASSWORD";
 $base_img_dir_sig = './images/bases/';
 
 //DBE
 $hostname_dbe = "localhost";
 $database_dbe = "DB_NAME";
 $username_dbe = "DB_USERNAME";
-$password_dbe = "DB_PASS";
+$password_dbe = "DB_PASSWORD";
 
 //STEAMTRACKS
 $hostname_steamtracks = "localhost";
 $database_steamtracks = "DB_NAME";
 $username_steamtracks = "DB_USERNAME";
-$password_steamtracks = "DB_PASS";
-$steamtracks_api_key = 'XXXXXXXXXXXXXXXXXXXX';
+$password_steamtracks = "DB_PASSWORD";
+$steamtracks_api_key = 'XXXXXXXXXXXXXXXXXX';
 $steamtracks_api_secret = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
 
-$steam_api_key = $api_key6;
-$steam_api_domain = "getdotastats.com";
+$steam_api_key = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+$steam_api_domain = "XXXXXXXXXXX.XXX";
 
 //BACKPACKS
 $hostname_backpacks = "localhost";
 $database_backpacks = "DB_NAME";
 $username_backpacks = "DB_USERNAME";
-$password_backpacks = "DB_PASS";
+$password_backpacks = "DB_PASSWORD";
 
-//STEAM API KEYS
-$api_key1 = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
-$api_key2 = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
-$api_key3 = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
-$api_key4 = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
-$api_key5 = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
-$api_key6 = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
-$api_key7 = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
-$api_key8 = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+//D2MODDIN
+$hostname_d2moddin = "localhost";
+$database_d2moddin = "DB_NAME";
+$username_d2moddin = "DB_USERNAME";
+$password_d2moddin = "DB_PASSWORD";
+
+$d2moddin_mongo_host = 'XXXXXX.XXXXX.XX';
+$d2moddin_mongo_database = 'DB_NAME';
+$d2moddin_mongo_username = 'DB_USERNAME';
+$d2moddin_mongo_password = 'DB_PASSWORD';
+
+//GDS Feeds
+$hostname_gds_feeds = "localhost";
+$database_gds_feeds = "DB_NAME";
+$username_gds_feeds = "DB_USERNAME";
+$password_gds_feeds = 'DB_PASSWORD';
+
+//GDS Site
+$hostname_gds_site = "localhost";
+$database_gds_site = "DB_NAME";
+$username_gds_site = "DB_USERNAME";
+$password_gds_site = 'DB_PASSWORD';
+
+//TEST LANDING PAGE
+$hostname_gds_test = "localhost";
+$database_gds_test = "DB_NAME";
+$username_gds_test = "DB_USERNAME";
+$password_gds_test = 'DB_PASSWORD';
+
+//GDS Cron
+$hostname_gds_cron = "localhost";
+$database_gds_cron = "DB_NAME";
+$username_gds_cron = "DB_USERNAME";
+$password_gds_cron = 'DB_PASSWORD';
+
+//Steam API Keys
+$api_key1 = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'; 
+$api_key2 = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'; 
+$api_key3 = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'; 
+$api_key4 = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'; 
+$api_key5 = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'; 
+$api_key6 = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'; 
+$api_key7 = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'; 
+$api_key8 = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'; 
 
 $api_key_dbe = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
