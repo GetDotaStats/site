92c38a793db478f6f7b3c4fa280a04a6d2ce793d
jimmydorry <your.name@domain>
2015-09-25 17:52:52 +1000

    SQL schema update

--- sql/s2/match-schema.sql
+++ sql/s2/match-schema.sql
@@ -46,7 +46,7 @@ CREATE TABLE IF NOT EXISTS `s2_match_custom` (
   `schemaID` int(255) NOT NULL,
   `round` tinyint(1) NOT NULL,
   `fieldOrder` tinyint(1) NOT NULL,
-  `fieldValue` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `fieldValue` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`matchID`,`round`,`fieldOrder`),
   KEY `index_mod_schema_round_order` (`modID`,`schemaID`,`round`,`fieldOrder`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -91,41 +91,11 @@ CREATE TABLE IF NOT EXISTS `s2_match_players_name` (
   `steamID32` bigint(255) NOT NULL,
   `steamID64` bigint(255) NOT NULL,
   `playerName` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `playerVanity` varchar(100) DEFAULT NULL,
   `dateUpdated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`steamID32`),
   KEY `index_steam_id64` (`steamID64`),
   KEY `index_player_name` (`playerName`),
-  KEY `index_date_updated` (`dateUpdated`)
+  KEY `index_date_updated` (`dateUpdated`),
+  KEY `index_player_vanity` (`playerVanity`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE IF NOT EXISTS `s2_mod_custom_schema` (
-  `schemaID` int(255) NOT NULL AUTO_INCREMENT,
-  `modID` int(255) NOT NULL,
-  `schemaAuth` varchar(16) NOT NULL,
-  `schemaVersion` int(11) NOT NULL,
-  `schemaApproved` tinyint(1) NOT NULL,
-  `schemaRejected` tinyint(1) NOT NULL,
-  `schemaRejectedReason` text,
-  `schemaSubmitterUserID64` bigint(20) NOT NULL,
-  `schemaApproverUserID64` bigint(20) DEFAULT NULL,
-  `dateRecorded` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`schemaID`),
-  UNIQUE KEY `index_modID_version` (`modID`,`schemaVersion`),
-  UNIQUE KEY `index_schemaAuth` (`schemaAuth`),
-  KEY `index_dateRecorded` (`dateRecorded`),
-  KEY `index_schemaApproved` (`schemaApproved`),
-  KEY `index_schemaRejected` (`schemaRejected`),
-  KEY `index_schemaSubmitterUserID64` (`schemaSubmitterUserID64`),
-  KEY `index_schemaApproverUserID64` (`schemaApproverUserID64`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
-
-CREATE TABLE IF NOT EXISTS `s2_mod_custom_schema_fields` (
-  `schemaID` int(255) NOT NULL,
-  `fieldType` tinyint(1) NOT NULL,
-  `fieldOrder` tinyint(1) NOT NULL,
-  `customValueObjective` tinyint(1) NOT NULL,
-  `customValueDisplay` varchar(100) COLLATE utf8_bin NOT NULL,
-  `customValueName` varchar(100) COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`schemaID`,`fieldType`,`fieldOrder`),
-  KEY `index_schema_objective` (`schemaID`,`customValueObjective`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
