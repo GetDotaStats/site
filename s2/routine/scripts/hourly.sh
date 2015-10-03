471e2aef83cc69c1afaba8b4a5e4b32cc311aa82
jimmydorry <your.name@domain>
2015-10-03 18:48:39 +1000

    Flag combo routine

---
+++ s2/routine/scripts/hourly.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /home/www-getdotastats/s2/routine/php/
+echo "Run at: $(date -u)<br />"
+echo "-------------------<br />"
+./custom_flags.php
+echo "-------------------<br />"
+echo "Ended at: $(date -u)"
