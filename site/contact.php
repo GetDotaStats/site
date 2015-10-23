d7c8301b3cb9fb9eab6cc84a699f953d2638d898
jimmydorry <jimmydorry+github@gmail.com>
2015-10-23 15:29:03 +1000

    Update contact details

--- site/contact.php
+++ site/contact.php
@@ -1,19 +1,17 @@
-<h3>Contact</h3>
+<h2>Official Contact Channels</h2>
 
-<p>Most of what you see wouldn't have been possible without the hard work put in by
-    <a target="_blank" href="https://github.com/SinZ163">SinZ</a>,
-    <a target="_blank" href="https://github.com/bmddota">BMD</a>,
-    <a target="_blank" href="https://github.com/ash47">Ash47</a>, and
-    <a target="_blank" href="https://github.com/jimmydorry">jimmydorry</a>.</p>
+<p>There are numerous ways to get in contact with us. We recommend you try IRC first, as there are plenty of helpful
+    people in there at all times. The Github Issue trackers are a great choice too!</p>
 
 <span class="h4">&nbsp;</span>
 
 <div class="text-center">
     <a target="_blank" class="btn btn-danger btn-sm" href="https://kiwiirc.com/client/irc.gamesurge.net/?#getdotastats">IRC
         webclient</a>
-    <a target="_blank" class="btn btn-danger btn-sm" href="irc://irc.gamesurge.net:6667/#getdotastats">IRC
-        #getdotastats</a>
-    <a target="_blank" class="btn btn-danger btn-sm" href="https://github.com/GetDotaStats/site/issues/new">Github
+    <a target="_blank" class="btn btn-danger btn-sm" href="irc://irc.gamesurge.net:6667/#getdotastats">IRC protocol</a>
+    <a target="_blank" class="btn btn-danger btn-sm" href="https://github.com/GetDotaStats/site/issues/new">Site
+        Issues</a>
+    <a target="_blank" class="btn btn-danger btn-sm" href="https://github.com/GetDotaStats/stat-collection/issues/new">stat-collection
         Issues</a>
     <a target="_blank" class="btn btn-danger btn-sm" href="http://steamcommunity.com/id/jimmydorry/">Steam</a>
     <a target="_blank" class="btn btn-danger btn-sm"
@@ -22,13 +20,7 @@
 
 <span class="h4">&nbsp;</span>
 
-<p>I don't add randoms on steam, so leave a comment before adding me.</p>
+<p>If you insist in getting in contact via Steam, make sure to leave a profile comment. My invite list gets flooded with
+    random invites!</p>
 
 <p>Comments and site feedback can also be left in the ChatBox to the left.</p>
-
-<span class="h4">&nbsp;</span>
-
-<p>If you like the work we do, feel free to shout us a beer via <a
-        href="bitcoin:1EFa8YBQAfM3s4pSjZEMyobY1f5H34nGBz?message=donation+to+getdotastats">1EFa8YBQAfM3s4pSjZEMyobY1f5H34nGBz</a>
-    or via our <a target="_blank" href="https://www.changetip.com/tipme/getdotastats">changetip</a> account.</em>
-</p>
