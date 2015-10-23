c34a1fa6a03426fcdff00a279cc028afd75a3405
jimmydorry <jimmydorry+github@gmail.com>
2015-10-23 15:37:09 +1000

    Add hidden recent_games filter for phase

--- s2/recent_games.php
+++ s2/recent_games.php
@@ -13,8 +13,27 @@ try {
     $memcache = new Memcache;
     $memcache->connect("localhost", 11211); # You might need to set "localhost" to "127.0.0.1"
 
+    $filterPhase = !empty($_GET['p']) && is_numeric($_GET['p'])
+        ? $_GET['p']
+        : -1;
+
+    switch ($filterPhase) {
+        case 1:
+            $filterPhaseSQL = ' AND s2.`matchPhaseID` = 1';
+            break;
+        case 2:
+            $filterPhaseSQL = ' AND s2.`matchPhaseID` = 2';
+            break;
+        case 3:
+            $filterPhaseSQL = ' AND s2.`matchPhaseID` = 3';
+            break;
+        default:
+            $filterPhaseSQL = '';
+            break;
+    }
+
     $recentGames = cached_query(
-        's2_recent_games',
+        's2_recent_games_p' . $filterPhase,
         'SELECT
               s2.`matchID`,
               s2.`matchAuthKey`,
@@ -35,7 +54,7 @@ try {
               ml.`mod_workshop_link`
             FROM `s2_match` s2
             JOIN `mod_list` ml ON s2.`modID` = ml.`mod_id`
-            WHERE ml.`mod_active` = 1
+            WHERE ml.`mod_active` = 1' . $filterPhaseSQL . '
             ORDER BY s2.`dateRecorded` DESC
             LIMIT 0,30;',
         NULL,
@@ -69,12 +88,16 @@ try {
             </div>';
 
     foreach ($recentGames as $key => $value) {
+        $modName = strlen($value['mod_name']) > 19
+            ? substr($value['mod_name'], 0, 19)
+            : $value['mod_name'];
+
         echo '<div class="row searchRow">
                 <a class="nav-clickable" href="#s2__match?id=' . $value['matchID'] . '">
                     <div class="col-md-1"><span class="glyphicon glyphicon-eye-open"></span></div>
                 </a>
                 <a class="nav-clickable" href="#s2__mod?id=' . $value['modID'] . '">
-                    <div class="col-md-3"><span class="glyphicon glyphicon-eye-open"></span> ' . $value['mod_name'] . '</div>
+                    <div class="col-md-3"><span class="glyphicon glyphicon-eye-open"></span> ' . $modName . '</div>
                 </a>
                 <a class="nav-clickable" href="#s2__match?id=' . $value['matchID'] . '">
                     <div class="col-md-6">
