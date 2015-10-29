d05389909475f0f32c93606bc33966f6fd8b60b4
jimmydorry <jimmydorry+github@gmail.com>
2015-10-29 16:41:46 +1000

    Re-order service stats page

--- admin/service_stats.php
+++ admin/service_stats.php
@@ -24,70 +24,9 @@ try {
     $adminCheck = adminCheck($_SESSION['user_id64'], 'admin');
     if (empty($adminCheck)) throw new Exception('Not an admin!');
 
-
-    try {
-        echo '<h3>Recent Service Records</h3>';
-        echo '<p>The last 15 records.</p>';
-
-        $lastServiceRuns = cached_query(
-            'admin_last_10_service_runs',
-            'SELECT
-                    `instance_id`,
-                    `service_name`,
-                    `execution_time`,
-                    `performance_index1`,
-                    `performance_index2`,
-                    `performance_index3`,
-                    `date_recorded`
-                FROM `cron_services`
-                ORDER BY `date_recorded` DESC
-                LIMIT 0,15;',
-            NULL,
-            NULL,
-            5
-        );
-
-        if (empty($lastServiceRuns)) throw new Exception('No recorded service runs!');
-
-        echo "<div class='row'>
-                    <div class='col-md-3'><strong>Service</strong></div>
-                    <div class='col-md-7'>
-                        <div class='row'>
-                            <div class='col-md-3 text-center'><strong>Execution</strong></div>
-                            <div class='col-md-3 text-center'><strong>P. Index1</strong></div>
-                            <div class='col-md-3 text-center'><strong>P. Index2</strong></div>
-                            <div class='col-md-3 text-center'><strong>P. Index3</strong></div>
-                        </div>
-                    </div>
-                    <div class='col-md-2'><strong>Recorded</strong></div>
-                </div>";
-
-        echo '<span class="h4">&nbsp;</span>';
-
-        foreach ($lastServiceRuns as $key => $value) {
-            $dateRecorded = relative_time_v3($value['date_recorded']);
-            $executionTime = $value['execution_time'];
-            $performanceIndex1 = number_format($value['performance_index1']);
-            $performanceIndex2 = number_format($value['performance_index2']);
-            $performanceIndex3 = number_format($value['performance_index3']);
-
-            echo "<div class='row'>
-                    <div class='col-md-3'>{$value['service_name']}</div>
-                    <div class='col-md-7'>
-                        <div class='row'>
-                            <div class='col-md-3 text-right'>{$executionTime} sec</div>
-                            <div class='col-md-3 text-right'>{$performanceIndex1}</div>
-                            <div class='col-md-3 text-right'>{$performanceIndex2}</div>
-                            <div class='col-md-3 text-right'>{$performanceIndex3}</div>
-                        </div>
-                    </div>
-                    <div class='col-md-2 text-right'>{$dateRecorded}</div>
-                </div>";
-        }
-    } catch (Exception $e) {
-        echo formatExceptionHandling($e);
-    }
-
+    ////////////////////////////////////
+    //Execution time of Services
+    ////////////////////////////////////
     try {
         echo '<h3>Execution time of Services</h3>';
         echo '<p>How long services have taken to execute over the last month.</p>';
@@ -145,6 +84,71 @@ try {
         echo formatExceptionHandling($e);
     }
 
+    ////////////////////////////////////
+    //Recent Service Records
+    ////////////////////////////////////
+    try {
+        echo '<h3>Recent Service Records</h3>';
+        echo '<p>The last 30 records recorded against various services that we run.</p>';
+
+        $lastServiceRuns = cached_query(
+            'admin_last_30_service_runs',
+            'SELECT
+                    `instance_id`,
+                    `service_name`,
+                    `execution_time`,
+                    `performance_index1`,
+                    `performance_index2`,
+                    `performance_index3`,
+                    `date_recorded`
+                FROM `cron_services`
+                ORDER BY `date_recorded` DESC
+                LIMIT 0,30;',
+            NULL,
+            NULL,
+            5
+        );
+
+        if (empty($lastServiceRuns)) throw new Exception('No recorded service runs!');
+
+        echo "<div class='row'>
+                    <div class='col-md-3'><strong>Service</strong></div>
+                    <div class='col-md-7'>
+                        <div class='row'>
+                            <div class='col-md-3 text-center'><strong>Execution</strong></div>
+                            <div class='col-md-3 text-center'><strong>P. Index1</strong></div>
+                            <div class='col-md-3 text-center'><strong>P. Index2</strong></div>
+                            <div class='col-md-3 text-center'><strong>P. Index3</strong></div>
+                        </div>
+                    </div>
+                    <div class='col-md-2'><strong>Recorded</strong></div>
+                </div>";
+
+        echo '<span class="h4">&nbsp;</span>';
+
+        foreach ($lastServiceRuns as $key => $value) {
+            $dateRecorded = relative_time_v3($value['date_recorded']);
+            $executionTime = $value['execution_time'];
+            $performanceIndex1 = number_format($value['performance_index1']);
+            $performanceIndex2 = number_format($value['performance_index2']);
+            $performanceIndex3 = number_format($value['performance_index3']);
+
+            echo "<div class='row'>
+                    <div class='col-md-3'>{$value['service_name']}</div>
+                    <div class='col-md-7'>
+                        <div class='row'>
+                            <div class='col-md-3 text-right'>{$executionTime} sec</div>
+                            <div class='col-md-3 text-right'>{$performanceIndex1}</div>
+                            <div class='col-md-3 text-right'>{$performanceIndex2}</div>
+                            <div class='col-md-3 text-right'>{$performanceIndex3}</div>
+                        </div>
+                    </div>
+                    <div class='col-md-2 text-right'>{$dateRecorded}</div>
+                </div>";
+        }
+    } catch (Exception $e) {
+        echo formatExceptionHandling($e);
+    }
 
     echo '<span class="h4">&nbsp;</span>';
 
