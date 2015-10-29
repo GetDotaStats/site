87e6efe8d699db3c841d20633a2e620f80faf8ac
jimmydorry <jimmydorry+github@gmail.com>
2015-10-29 11:19:01 +1000

    Match overview enhancements

--- s2/match.php
+++ s2/match.php
@@ -59,12 +59,11 @@ try {
         throw new Exception('Invalid matchID! Not recorded in database.');
     }
 
-    echo '<h2><a class="nav-clickable" href="#s2__mod?id=' . $matchDetails[0]['modID'] . '">' . $matchDetails[0]['mod_name'] . '</a> <small>' . $matchID . '</small></h2>';
+    $hostUserID32 = $matchDetails[0]['matchHostSteamID32'];
+    $numRounds = $matchDetails[0]['numRounds'];
+    $numPlayers = $matchDetails[0]['numPlayers'];
 
-    //FEATURE REQUEST
-    echo '<div class="alert alert-danger"><strong>Help Wanted!</strong> We are re-designing every page. If there are features you would like to
-        see on this page, please let us know by making a post per feature on this page\'s
-        <a target="_blank" href="https://github.com/GetDotaStats/site/issues/164">issue</a>.</div>';
+    echo '<h2><a class="nav-clickable" href="#s2__mod?id=' . $matchDetails[0]['modID'] . '">' . $matchDetails[0]['mod_name'] . '</a> <small>' . $matchID . '</small></h2>';
 
     !empty($matchDetails[0]['mod_workshop_link'])
         ? $links['steam_workshop'] = '<a href="http://steamcommunity.com/sharedfiles/filedetails/?id=' . $matchDetails[0]['mod_workshop_link'] . '" target="_new"><span class="glyphicon glyphicon-new-window"></span> Workshop</a>'
@@ -86,49 +85,6 @@ try {
                     </div>
                 </div>
             </div>';
-
-    $clientDetailsPanel = '';
-    if (!empty($_SESSION['user_id64'])) {
-        //if admin, show modIdentifier too
-        $adminCheck = adminCheck($_SESSION['user_id64'], 'admin');
-        if (!empty($adminCheck)) {
-            $clientDetails = cached_query(
-                's2_client_details' . $matchID,
-                'SELECT
-                      `matchID`,
-                      `modID`,
-                      `steamID32`,
-                      `steamID64`,
-                      `clientIP`,
-                      `isHost`,
-                      `dateRecorded`
-                    FROM `s2_match_client_details`
-                    WHERE `matchID` = ?;',
-                's',
-                $matchID,
-                5
-            );
-
-            if (!empty($clientDetails)) {
-                $clientArray = array();
-                foreach ($clientDetails as $key => $value) {
-                    $clientIPvalue = $value['clientIP'];
-                    $clientIPvalue .= $value['isHost'] == 1
-                        ? ' (HOST)'
-                        : '';
-
-                    $clientArray[] = $clientIPvalue;
-                }
-
-                $clientDetailsPanel = implode('<br />', $clientArray);
-                $clientDetailsPanel = "<div class='row mod_info_panel'>
-                        <div class='col-sm-3'><strong>Client IPs</strong></div>
-                        <div class='col-sm-9'>{$clientDetailsPanel}</div>
-                    </div>";
-            }
-        }
-    }
-
     echo '<div id="mod_info" class="collapse col-sm-7">
                 <div class="row mod_info_panel">
                     <div class="col-sm-3"><strong>Links</strong></div>
@@ -137,7 +93,7 @@ try {
                 <div class="row mod_info_panel">
                     <div class="col-sm-3"><strong>Description</strong></div>
                     <div class="col-sm-9">' . $matchDetails[0]['mod_description'] . '</div>
-                </div>' . $clientDetailsPanel . '
+                </div>
            </div>';
     echo '</div>';
 
@@ -173,14 +129,14 @@ try {
                 </div>
             </div>';
 
-    //PLAYERS
+    $matchSummary = array();
+
+    //PLAYER DETAILS
     {
         $playerDetails = cached_query(
             's2_player_match_details' . $matchID,
             'SELECT
-                    s2mp.`matchID`,
                     s2mp.`roundID`,
-                    s2mp.`modID`,
                     s2mp.`steamID32`,
                     s2mp.`steamID64`,
                     s2mp.`connectionState`,
@@ -188,10 +144,13 @@ try {
 
                     s2mpn.`playerName`,
 
-                    gu.`user_avatar`
+                    gu.`user_avatar`,
+
+                    gcs.`cs_name` AS `connectionStateName`
                 FROM `s2_match_players` s2mp
                 LEFT JOIN `s2_match_players_name` s2mpn ON s2mp.`steamID32` = s2mpn.`steamID32`
                 LEFT JOIN `gds_users` gu ON s2mp.`steamID32` = gu.`user_id32`
+                LEFT JOIN `game_connection_status` gcs ON s2mp.`connectionState` = gcs.`cs_id`
                 WHERE s2mp.`matchID` = ?
                 ORDER BY s2mp.`roundID` ASC, s2mp.`steamID32` ASC;',
             's',
@@ -199,76 +158,18 @@ try {
             5
         );
 
-        if (empty($playerDetails)) {
-            throw new Exception('No player data recorded!');
-        }
-
-        $lastRound = -1;
-        foreach ($playerDetails as $key => $value) {
-            if ($value['roundID'] != $lastRound) {
-                echo '<span class="h4">&nbsp;</span>';
-
-                echo '<div class="row"><div class="col-md-4 h4">Round #' . $value['roundID'] . '</div></div>';
-                echo '<div class="row">
-                        <div class="col-md-4"><strong>Player</strong></div>
-                        <div class="col-md-1 text-center"><strong>Winner</strong></div>
-                        <div class="col-md-2 text-center"><strong>Connection</strong></div>
-                    </div>';
-
-
-                $lastRound = $value['roundID'];
+        if (!empty($playerDetails)) {
+            foreach ($playerDetails as $key => $value) {
+                $matchSummary[$value['roundID']]['players'][$value['steamID32']]['steamID32'] = $value['steamID32'];
+                $matchSummary[$value['roundID']]['players'][$value['steamID32']]['steamID64'] = $value['steamID64'];
+                $matchSummary[$value['roundID']]['players'][$value['steamID32']]['isWinner'] = $value['isWinner'];
+                $matchSummary[$value['roundID']]['players'][$value['steamID32']]['playerName'] = $value['playerName'];
+                $matchSummary[$value['roundID']]['players'][$value['steamID32']]['userAvatar'] = $value['user_avatar'];
+                $matchSummary[$value['roundID']]['players'][$value['steamID32']]['connectionState'] = $value['connectionState'];
+                $matchSummary[$value['roundID']]['players'][$value['steamID32']]['connectionStateName'] = $value['connectionStateName'];
             }
-
-            $userAvatar = !empty($value['user_avatar'])
-                ? $value['user_avatar']
-                : $CDN_image . '/images/misc/steam/blank_avatar.jpg';
-            $userAvatarLink = '<a target="_blank" href="https://steamcommunity.com/profiles/' . $value['steamID64'] . '"><img src="' . $userAvatar . '" width="14" /></a>';
-
-            $usernameLink = !empty($value['playerName'])
-                ? $value['playerName']
-                : '????';
-            $usernameLink = '<a class="nav-clickable" href="#s2__user?id=' . $value['steamID64'] . '">' . $usernameLink . '</a>';
-
-            $isWinner = $value['isWinner'] == 1
-                ? '<span class="glyphicon glyphicon-ok boldGreenText"></span>'
-                : '<span class="glyphicon glyphicon-remove boldRedText"></span>';
-
-            echo '<div class="row">
-                        <div class="col-md-4">' . $userAvatarLink . ' ' . $usernameLink . '</div>
-                        <div class="col-md-1 text-center">' . $isWinner . '</div>
-                        <div class="col-md-2 text-center">' . $value['connectionState'] . '</div>
-                    </div>';
         }
     }
-
-    echo '<span class="h4">&nbsp;</span>';
-
-    //FLAGS
-    {
-        $flagsDetails = cached_query(
-            's2_flags_match_details' . $matchID,
-            'SELECT
-                    s2mf.`flagName`,
-                    s2mf.`flagValue`
-                FROM `s2_match_flags` s2mf
-                WHERE s2mf.`matchID` = ?
-                ORDER BY s2mf.`flagName` ASC;',
-            's',
-            $matchID,
-            5
-        );
-
-        if (!empty($flagsDetails)) {
-            echo '<h4>Flags Dump</h4>';
-            echo '<p>I\'m sorry</p>';
-
-            echo '<pre>';
-            print_r($flagsDetails);
-            echo '</pre>';
-        }
-    }
-
-    echo '<span class="h4">&nbsp;</span>';
 
     //CUSTOM FIELDS GAME
     {
@@ -293,23 +194,11 @@ try {
         );
 
         if (!empty($customGameDetails)) {
-            echo '<h4>Custom Game Values Dump</h4>';
-            echo '<p>I\'m sorry</p>';
-
-            $tempArray = array();
-
             foreach ($customGameDetails as $key => $value) {
-                $tempArray[$value['round']][$value['customValueDisplay']] = $value['fieldValue'];
+                $matchSummary[$value['round']]['cgv'][$value['customValueDisplay']] = $value['fieldValue'];
             }
-
-            echo '<pre>';
-            print_r($tempArray);
-            echo '</pre>';
-            unset($tempArray);
         }
     }
-
-    echo '<span class="h4">&nbsp;</span>';
 
     //CUSTOM FIELDS PLAYER
     {
@@ -335,24 +224,198 @@ try {
         );
 
         if (!empty($customPlayerDetails)) {
-            echo '<h4>Custom Player Values Dump</h4>';
-            echo '<p>I\'m sorry</p>';
-
-            $tempArray = array();
-
             foreach ($customPlayerDetails as $key => $value) {
-                $tempArray[$value['round']][$value['userID32']][$value['customValueDisplay']] = $value['fieldValue'];
+                if (!isset($matchSummary[$value['round']]['players'][$value['userID32']])) throw new Exception('No basic stats for user ' . $value['userID32'] . ' in round #' . $value['round'] . '!');
+                $matchSummary[$value['round']]['players'][$value['userID32']]['cpv'][$value['customValueDisplay']] = $value['fieldValue'];
+            }
+        }
+    }
+
+    //Client IPs
+    {
+        if (!empty($_SESSION['user_id64'])) {
+            //if admin, show clientIPs too
+            $adminCheck = adminCheck($_SESSION['user_id64'], 'admin');
+            if (!empty($adminCheck)) {
+                $clientDetails = cached_query(
+                    's2_client_details' . $matchID,
+                    'SELECT
+                          `matchID`,
+                          `modID`,
+                          `steamID32`,
+                          `steamID64`,
+                          `clientIP`,
+                          `isHost`,
+                          `dateRecorded`
+                        FROM `s2_match_client_details`
+                        WHERE `matchID` = ?;',
+                    's',
+                    $matchID,
+                    5
+                );
+
+                if (!empty($clientDetails)) {
+                    foreach ($clientDetails as $key => $value) {
+                        if (!isset($matchSummary[1]['players'][$value['steamID32']])) throw new Exception('No basic stats for user ' . $value['steamID32'] . ' in round #1!');
+                        $matchSummary[1]['players'][$value['steamID32']]['cpv']['IP'] = $value['clientIP'];
+                    }
+                }
+            }
+        }
+    }
+
+    if (empty($matchSummary)) throw new Exception('No player details recorded!');
+
+    //Iterate over all of the rounds in the match summary array
+    foreach ($matchSummary as $key => $value) {
+        try {
+            //Only show round heading if there is more than 1 round
+            echo $numRounds > 1
+                ? '<h4>Round #' . $key . '</h4>'
+                : '<br />';
+
+            //Only show the custom GAME stats button if there is data to show
+            if (!empty($value['cgv'])) {
+                echo '<strong>Custom Game stats</strong> <button type="button" class="btn btn-default btn-xs" data-toggle="collapse" data-target="#custom_game_info_' . $key . '">show</button>';
+
+                echo '<div class="container">';
+                echo '<div id="custom_game_info_' . $key . '" class="collapse col-sm-5">';
+                echo '<br />';
+
+                //Iterate over all the custom stats for the round we are currently iterating over
+                foreach ($value['cgv'] as $key2 => $value2) {
+                    echo '<div class="row mod_info_panel">
+                                <div class="col-sm-4"><strong>' . $key2 . '</strong></div>
+                                <div class="col-sm-8">' . $value2 . '</div>
+                            </div>';
+                }
+
+                echo '<br />';
+                echo '</div>';
+                echo '</div>';
             }
 
-            echo '<pre>';
-            print_r($tempArray);
-            echo '</pre>';
-            unset($tempArray);
+
+            echo '<div class="row">
+                        <div class="col-md-4"><strong>Player</strong></div>
+                        <div class="col-md-1 text-center"><strong>Winner</strong></div>
+                        <div class="col-md-2 text-center"><strong>Connection</strong></div>
+                    </div>';
+
+            if (empty($value['players'])) throw new Exception("No basic stats for this round #{$key}!");
+
+            //Iterate over all of the players for this round in the match summary array
+            foreach ($value['players'] as $key2 => $value2) {
+                $userAvatar = !empty($value2['user_avatar'])
+                    ? $value2['user_avatar']
+                    : $CDN_image . '/images/misc/steam/blank_avatar.jpg';
+                $userAvatarLink = '<a target="_blank" href="https://steamcommunity.com/profiles/' . $value2['steamID64'] . '"><img src="' . $userAvatar . '" width="14" /></a>';
+
+                $usernameLink = !empty($value2['playerName'])
+                    ? $value2['playerName']
+                    : '????';
+                $usernameLink = '<a class="nav-clickable" href="#s2__user?id=' . $value2['steamID64'] . '">' . $usernameLink . '</a>';
+
+                $isWinner = $value2['isWinner'] == 1
+                    ? '<span class="glyphicon glyphicon-ok boldGreenText"></span>'
+                    : '<span class="glyphicon glyphicon-remove boldRedText"></span>';
+
+                $isHost = $value2['steamID32'] == $hostUserID32
+                    ? ' <span class="glyphicon glyphicon-asterisk" title="This player was the host"></span>'
+                    : '';
+
+                $goodConnectionStates = array(1, 2, 3);
+                $connectionState = in_array($value2['connectionState'], $goodConnectionStates)
+                    ? '<span class="glyphicon glyphicon-ok boldGreenText" title="' . $value2['connectionStateName'] . '"></span>'
+                    : '<span class="glyphicon glyphicon-remove boldRedText" title="' . $value2['connectionStateName'] . '"></span>';
+
+                //Only show the custom stats button if there is data to show
+                $moreInfoButton = !empty($value2['cpv'])
+                    ? '<div class="col-md-2 text-center"><button type="button" class="btn btn-default btn-xs" data-toggle="collapse" data-target="#player_info_' . $key . '_' . $value2['steamID32'] . '">Info</button></div>'
+                    : '';
+
+                echo '<div class="row">
+                            <div class="col-md-4">' . $userAvatarLink . ' ' . $usernameLink . $isHost . '</div>
+                            <div class="col-md-1 text-center">' . $isWinner . '</div>
+                            <div class="col-md-2 text-center">' . $connectionState . '</div>' . $moreInfoButton . '
+                        </div>';
+
+                if (!empty($value2['cpv'])) {
+                    echo '<div class="container">';
+                    echo '<div id="player_info_' . $key . '_' . $value2['steamID32'] . '" class="collapse col-sm-9">';
+                    echo '<br />';
+
+                    $customStats = '<div class="row mod_info_panel">';
+                    //Iterate over all the custom stats for the player we are currently iterating over
+                    $i = 1;
+                    $numStats = count($value2['cpv']);
+                    foreach ($value2['cpv'] as $key3 => $value3) {
+                        $customStats .= '<div class="col-sm-2"><strong>' . $key3 . '</strong></div>
+                                    <div class="col-sm-2">' . $value3 . '</div>';
+
+                        if ($i % 3 == 0 && $i > 1 && $i != $numStats) {
+                            $customStats .= '</div><div class="row mod_info_panel">';
+                        }
+
+                        $i++;
+                    }
+
+                    if ($i % 3 <> 0 && $i > 1) {
+                        $customStats .= '</div>';
+                    }
+
+                    echo $customStats;
+
+                    echo '<br />';
+                    echo '</div>';
+                    echo '</div>';
+                }
+            }
+
+            echo '<span class="h4">&nbsp;</span>';
+        } catch (Exception $e) {
+            echo formatExceptionHandling($e);
+        }
+    }
+
+
+    echo '<span class="h4">&nbsp;</span>';
+
+    //FLAGS
+    {
+        $flagsDetails = cached_query(
+            's2_flags_match_details' . $matchID,
+            'SELECT
+                    s2mf.`flagName`,
+                    s2mf.`flagValue`
+                FROM `s2_match_flags` s2mf
+                WHERE s2mf.`matchID` = ?
+                ORDER BY s2mf.`flagName` ASC;',
+            's',
+            $matchID,
+            5
+        );
+
+        if (!empty($flagsDetails)) {
+            echo '<div class="row">
+                    <div class="col-sm-2"><strong>Flag</strong></div>
+                    <div class="col-sm-3"><strong>Value</strong></div>
+                </div>';
+
+            foreach ($flagsDetails as $key => $value) {
+                echo '<div class="row">
+                    <div class="col-sm-2">' . $value['flagName'] . '</div>
+                    <div class="col-sm-3">' . $value['flagValue'] . '</div>
+                </div>';
+            }
         }
     }
 
     echo '<hr />';
-
+} catch (Exception $e) {
+    echo formatExceptionHandling($e);
+} finally {
+    if (isset($memcache)) $memcache->close();
 
     echo '<span class="h4">&nbsp;</span>';
 
@@ -362,8 +425,4 @@ try {
            </div>';
 
     echo '<span class="h4">&nbsp;</span>';
-} catch (Exception $e) {
-    echo formatExceptionHandling($e);
-} finally {
-    if (isset($memcache)) $memcache->close();
 }
