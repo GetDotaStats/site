c19d70ee34edba2699c1dcc25d7e21a5b3081a66
jimmydorry <jimmydorry@users.noreply.github.com>
2015-11-02 22:11:51 +1000

    Move files down a level for easier environment setup

---
+++ site_files/dbe/connections/parametersSAMPLE.php
@@ -0,0 +1,8 @@
+<?php
+$hostname = "localhost";
+$database = "DB_NAME";
+$username = "DB_USERNAME";
+$password = "DB_PASSWORD";
+
+$api_key1 = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+$api_key_dbe = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
