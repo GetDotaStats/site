0dde29944ac88decbfd22215c9319bb674d2396c
jimmydorry <jimmydorry@users.noreply.github.com>
2015-12-08 20:50:50 +1000

    Adventofcode December Challenge - Day8

---
+++ site_files/adventofcode/day8.php
@@ -0,0 +1,34 @@
+<?php
+//require_once('./functions.php');
+require_once('../connections/parameters.php');
+require_once('../global_functions.php');
+
+try {
+    $input_file = './day8_input.txt';
+    //if (!is_file($input_file)) throw new Exception("Input put file missing!");
+    //$input = file_get_contents($input_file);
+    //if (empty($input)) throw new Exception("No input!");
+
+    $part1 = $part2 = 0;
+
+    foreach (file($input_file, FILE_IGNORE_NEW_LINES) as $line) {
+        eval('$str = ' . $line . ';');
+        $part1 += strlen($line) - strlen($str);
+        $part2 += strlen(addslashes($line)) + 2 - strlen($line);
+    }
+
+    $result = "<strong>Part 1:</strong> Difference of #<strong>{$part1}</strong> characters.";
+    $result .= isset($part2)
+        ? "<br /><strong>Part 2:</strong> Difference of #<strong>{$part2}</strong>characters."
+        : '';
+
+    echo $result;
+} catch (Exception $e) {
+    $result = 'Caught Exception: ' . $e->getMessage() . ' on line ' . $e->getLine() . '<br />';
+    echo $result;
+} finally {
+    if (isset($memcache)) $memcache->close();
+    if (!isset($result)) {
+        echo 'Unknown error! #1' . '<br />';
+    }
+}
