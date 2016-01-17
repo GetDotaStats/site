f5b1ccab314248e1321c110b523185c98a32d98d
jimmydorry <jimmydorry@users.noreply.github.com>
2016-01-17 19:11:09 +1000

    Add time filter to workshop graphs

--- site_files/s2/mod_ws.php
+++ site_files/s2/mod_ws.php
@@ -19,6 +19,37 @@ try {
 
     $modID = $_GET['id'];
 
+    $filterTimeSpan = !empty($_GET['t']) && is_numeric($_GET['t'])
+        ? $_GET['t']
+        : -1;
+
+    switch ($filterTimeSpan) {
+        case 1:
+            $filterTimeSpanSQL = ' AND mw.`date_recorded` >= NOW() - INTERVAL 7 DAY ';
+            $sqlFilter = 1;
+            break;
+        case 2:
+            $filterTimeSpanSQL = ' AND mw.`date_recorded` >= NOW() - INTERVAL 30 DAY ';
+            $sqlFilter = 2;
+            break;
+        case 3:
+            $filterTimeSpanSQL = ' AND mw.`date_recorded` >= NOW() - INTERVAL 60 DAY ';
+            $sqlFilter = 3;
+            break;
+        case 4:
+            $filterTimeSpanSQL = ' AND mw.`date_recorded` >= NOW() - INTERVAL 180 DAY ';
+            $sqlFilter = 4;
+            break;
+        case 5:
+            $filterTimeSpanSQL = '';
+            $sqlFilter = 5;
+            break;
+        default:
+            $filterTimeSpanSQL = ' AND mw.`date_recorded` >= NOW() - INTERVAL 30 DAY ';
+            $sqlFilter = 2;
+            break;
+    }
+
     $db = new dbWrapper_v3($hostname_gds_site, $username_gds_site, $password_gds_site, $database_gds_site, true);
     if (empty($db)) throw new Exception('No DB!');
 
@@ -48,6 +79,14 @@ try {
                 echo formatExceptionHandling($e);
             }
 
+            echo '<div class="text-center">
+                    <a class="nav-clickable btn btn-sm btn-success" href="#s2__mod_ws?id=' . $modID . '&t=1">Last Week</a>
+                    <a class="nav-clickable btn btn-sm btn-success" href="#s2__mod_ws?id=' . $modID . '&t=2">Last Month</a>
+                    <a class="nav-clickable btn btn-sm btn-success" href="#s2__mod_ws?id=' . $modID . '&t=3">Last 2 Months</a>
+                    <a class="nav-clickable btn btn-sm btn-success" href="#s2__mod_ws?id=' . $modID . '&t=4">Last 6 Months</a>
+                    <a class="nav-clickable btn btn-sm btn-success" href="#s2__mod_ws?id=' . $modID . '&t=5">All Time</a>
+               </div>';
+
             $modDetails = cached_query(
                 's2_mod_ws_mod_details' . $modID,
                 'SELECT
@@ -68,32 +107,32 @@ try {
             $modName = $modDetails[0]['mod_name'];
 
             $workshopStats = cached_query(
-                's2_mod_ws_' . $modID,
+                's2_mod_ws_' . $modID . '_' . $sqlFilter,
                 'SELECT
-                      DAY(`date_recorded`) as `day`,
-                      MONTH(`date_recorded`) as `month`,
-                      YEAR(`date_recorded`) as `year`,
-                      `mod_check_id`,
-                      `mod_identifier`,
-                      `mod_workshop_id`,
-                      `mod_size`,
-                      `mod_hcontent_file`,
-                      `mod_hcontent_preview`,
-                      `mod_thumbnail`,
-                      `mod_views`,
-                      `mod_subs`,
-                      `mod_favs`,
-                      `mod_subs_life`,
-                      `mod_favs_life`,
-                      `date_last_updated`,
-                      `date_recorded`
-                    FROM `mod_workshop`
-                    WHERE `mod_identifier` = ?
+                      DAY(mw.`date_recorded`) as `day`,
+                      MONTH(mw.`date_recorded`) as `month`,
+                      YEAR(mw.`date_recorded`) as `year`,
+                      mw.`mod_check_id`,
+                      mw.`mod_identifier`,
+                      mw.`mod_workshop_id`,
+                      mw.`mod_size`,
+                      mw.`mod_hcontent_file`,
+                      mw.`mod_hcontent_preview`,
+                      mw.`mod_thumbnail`,
+                      mw.`mod_views`,
+                      mw.`mod_subs`,
+                      mw.`mod_favs`,
+                      mw.`mod_subs_life`,
+                      mw.`mod_favs_life`,
+                      mw.`date_last_updated`,
+                      mw.`date_recorded`
+                    FROM `mod_workshop` mw
+                    WHERE mw.`mod_identifier` = ? ' . $filterTimeSpanSQL . '
                     GROUP BY 3,2,1
                     ORDER BY 3,2,1;',
                 's',
                 array($modIdentifier),
-                5
+                10
             );
 
             if (empty($workshopStats)) {
