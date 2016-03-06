2a9a41eccef150152a2983aea285a4ac36aa1341
jimmydorry <jimmydorry@users.noreply.github.com>
2016-03-06 23:20:34 +1000

    Cleanup old code

--- site_files/dbe/connections/parametersSAMPLE.php
+++
@@ -1,8 +0,0 @@
-<?php
-$hostname = "localhost";
-$database = "DB_NAME";
-$username = "DB_USERNAME";
-$password = "DB_PASSWORD";
-
-$api_key1 = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
-$api_key_dbe = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
